c7984cc3c405047edf23366cc6ac855343eb13c6
Eric Bidelman <ebidel@gmail.com>
2019-01-07 15:22:51 -0800

    Staging server: renames / cleanup (#546)
    * updates to names, docstrings, etc
    
    * staging server: cleanup
    
    * fix typo

--- server/render.mjs
+++ server/render.mjs
@@ -25,53 +25,48 @@ import url from 'url';
 const URL = url.URL;
 import puppeteer from 'puppeteer';
 
-const DOMAIN = 'https://web.dev/';
+const SITE_ORIGIN = 'https://web.dev';
 
 let browser = null;
 
 /**
- * Server-side renders a URL using headless chrome.
- * @param {string} url The url to prerender.
+ * Constructs a page by fetching it from web.dev, then merging it with the
+ * the local version on disk.
  * @param {string} path File path to render.
- * @param {!Object} config Optional config settings.
- *     path: Path of page to load.
+ * @param {!Object} options Optional config settings.
  *     stagingServerOrigin: Origin of staging server.
- *     headless: Set to false to launch headlful chrome.xg
- * @return {!Promise<?string>} Serialized page output as an html string.
+ *     headless: False launches headful chrome instead of headless.
+ * @return {!Promise<?string>} Serialized page output as an html string or null
+ *     if the local page file does not exist.
  */
-export async function renderPage({
-  path = null,
+export async function constructPage(path, {
   stagingServerOrigin = null,
   headless = true,
 } = {}) {
-  const url = `${DOMAIN}${path}`;
-  const tic = Date.now();
+  path = path.slice(1); // strip leading '/'.
+  const url = `${SITE_ORIGIN}/${path}`;
 
   if (!browser) {
     browser = await puppeteer.launch({
-      // args: ['--disable-dev-shm-usage'],
       headless,
       // TODO: check if we need to consider defaultViewport for mobile.
       defaultViewport: null,
     });
   }
 
-  // Get local version of the page.
-  const pageHTML = await getPageContent(path);
-  if (!pageHTML) {
+  // Read local version of the page. Replace {% include %} with their content.
+  const localPageHTML = replaceIncludes(await readLocalPageContent(path));
+  if (!localPageHTML) {
     return null;
   }
 
-  // Replace local page's {% include %} with the actual html in those files.
-  const include = replaceIncludes(pageHTML);
-
-  const page = await getRemotePage(url);
+  const page = await fetchRemotePage(url);
 
   // Replace main body of the remote page with the local copy.
-  await page.evaluate((include) => {
+  await page.evaluate((localPageHTML) => {
     const mainContentArea = document.querySelector('.devsite-article-body');
     // Replace page with include filled in version.
-    mainContentArea.innerHTML = include;
+    mainContentArea.innerHTML = localPageHTML;
 
     // Map relative assets to their local version. Relative assets that do
     // not exist locally (e.g. /_static images) are pulled from the prod site.
@@ -90,15 +85,12 @@ export async function renderPage({
     // // when the page is loaded in the browser by the viewer.
     // // TODO: dont want to do this for relative links.
     // const base = document.createElement('base');
-    // base.href = `${origin}${location.pathname}/`;
+    // base.href = `${stagingServerOrigin}${location.pathname}/`;
     // document.head.prepend(base); // Add to top of head,
     //                              // before all other resources.
-  }, include);
+  }, localPageHTML);
 
-  const finalHTML = await page.content(); // get page's serialized, final DOM.
-
-  // eslint-disable-next-line
-  console.info(`Headless rendered ${url} in: ${Date.now() - tic}ms`);
+  const finalHTML = await page.content(); // serialized HTML of assembled page.
 
   await page.close();
 
@@ -109,7 +101,7 @@ export async function renderPage({
  * @param {string} url Page to fetch.
  * @return {!Promise<!Page>}
  */
-async function getRemotePage(url) {
+async function fetchRemotePage(url) {
   const page = await browser.newPage();
 
   // Add param so client-side page can know it's being rendered by headless
@@ -130,11 +122,10 @@ async function getRemotePage(url) {
     throw new Error('page.goto/waitForSelector timed out.');
   }
 
-  // If page is new (e.g. doesn't exist in production yet), need to wrap
-  // it in local content in a generic header/footer.
-  // Wrap it in index page's markup.
+  // If page is new (e.g. doesn't exist in production yet), need to wrap it in
+  // local content in a generic header/footer. Wrap it in index page's markup.
   if (resp.status() === 404) {
-    return await getRemotePage(DOMAIN);
+    return await fetchRemotePage(SITE_ORIGIN);
   }
 
   return page;
@@ -145,7 +136,7 @@ async function getRemotePage(url) {
  * @param {string} path
  * @return {!Promise<?string>}
  */
-async function getPageContent(path) {
+async function readLocalPageContent(path) {
   let filePath = `./build/en/${path}.html`;
   try {
     // If .html file doesn't exist, try folder's index.html file.
@@ -171,12 +162,16 @@ async function getPageContent(path) {
 
 /**
  * Replaces page's {% include "file.html" %} pragmas.
- * @param {string} pageHTML
- * @return {string}
+ * @param {?string} html
+ * @return {?string}
  */
-function replaceIncludes(pageHTML) {
+function replaceIncludes(html) {
+  if (!html) {
+    return null;
+  }
+
   const re = /{% include "(.*)\.html" %}/g;
-  return pageHTML.replace(re, (match, filename, offset) => {
+  return html.replace(re, (match, filename, offset) => {
     try {
       const html = fs.readFileSync(`./build/en/${filename}.html`, {
         encoding: 'utf-8',
