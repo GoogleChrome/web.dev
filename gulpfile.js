59026468d5a5631ac6a5112cdeaf50d063447ea3
Tim van der Lippe <tvanderlippe@google.com>
2019-01-07 12:31:55 +0000

    Ignore google-specific files

--- gulpfile.js
+++ gulpfile.js
@@ -57,7 +57,7 @@ gulp.task(
 gulp.task('lint', () => {
   return (
     gulp
-      .src(['{lib,server}/{*.js,*.mjs,!(deps)/**/*.js}', '!glitches/**/*.{js,mjs}'])
+      .src(['{lib,server}/{*.js,*.mjs,!(deps)/**/*.js}', '!glitches/**/*.{js,mjs}', '!lib/devsite.js', '!lib/local-devsite.js'])
       // eslint() attaches the lint output to the "eslint" property
       // of the file object so it can be used by other modules.
       .pipe(eslint())
