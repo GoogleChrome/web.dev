d181f36f22cbd056f78477b6e23214ba4555733b
Rob Dodson <robdodson@google.com>
2019-02-07 15:18:14 -0800

    Add initial sass.

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@
  */
 
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/scripts/main.ts',
@@ -41,4 +42,12 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new CopyWebpackPlugin([
+      {
+        from: path.join(__dirname, 'src', 'sandbox'),
+        to: path.join(__dirname, 'build', 'sandbox'),
+      },
+    ]),
+  ],
 };
