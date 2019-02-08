b18cdbb14a91f3b9697be77c0d84ab57214fb1dd
Sam Thorogood <sam.thorogood@gmail.com>
2019-02-08 11:40:04 +1100

    add simple gauge from internal

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,5 +1,6 @@
 import '../styles/main.scss';
 import '../components/web_progress_bar/web_progress_bar.ts';
+import '../components/web_lighthouse_gauge/web_lighthouse_gauge.ts';
 
 /* eslint-disable-next-line */
 console.log('hello world');
