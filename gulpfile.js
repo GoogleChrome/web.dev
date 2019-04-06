39b5e578a1adfd568e9c9d47d9f2e7bb5e961266
Rob Dodson <robdodson@google.com>
2019-04-06 16:23:23 -0700

    Fixup images and copying.

--- gulpfile.js
+++ gulpfile.js
@@ -46,16 +46,28 @@ gulp.task('scss', () => {
   );
 });
 
+// These are images that our CSS refers to and must be checked in to DevSite.
 gulp.task('copy-global-assets', () => {
   return gulp.src(['./src/images/**/*']).pipe(gulp.dest('./dist/images'));
 });
 
+// These are images that go with our landing pages. They live in the same
+// directory as our other eleventy content, but they shouldn't get their
+// directories stripped like we do for the `posts` directory.
+gulp.task('copy-landing-page-assets', () => {
+  return gulp
+    .src(['./src/site/content/en/images/**/*'])
+    .pipe(gulp.dest('./dist/en/images'));
+});
+
+// These are required yaml files, robots.txt, sitemap.xml, etc.
 gulp.task('copy-configuration-assets', () => {
   return gulp
-    .src(['./src/site/content/**/*.{yaml,txt}'])
+    .src(['./src/site/content/**/*.{yaml,txt,xml}'])
     .pipe(gulp.dest('./dist'));
 });
 
+// These are images that live with our pattern library.
 gulp.task('copy-sandbox-assets', () => {
   return gulp
     .src([`./src/site/content/sandbox/**/*.{${assetTypes}}`])
@@ -66,15 +78,23 @@ gulp.task('copy-sandbox-assets', () => {
 // we need to manually copy over content images ourselves.
 // Note that we only copy over the images in the en/ directory.
 // On the server we'll redirect localized docs to request images from en/.
-gulp.task('copy-content-assets', () => {
-  return gulp
-    .src(`./src/site/content/en/**/*.{${assetTypes}}`)
-    .pipe(
-      rename(function(assetPath) {
-        assetPath.dirname = path.basename(assetPath.dirname);
-      })
-    )
-    .pipe(gulp.dest('./dist/en'));
+gulp.task('copy-post-assets', () => {
+  return (
+    gulp
+      .src([
+        `./src/site/content/en/**/*.{${assetTypes}}`,
+        '!./src/site/content/en/images/**/*', // leave the images dir alone
+      ])
+      // Removes the `posts` directory from the image output path.
+      // This makes the images show up in the same spot as the permalinked posts
+      // they belong to.
+      .pipe(
+        rename(function(assetPath) {
+          assetPath.dirname = path.basename(assetPath.dirname);
+        })
+      )
+      .pipe(gulp.dest('./dist/en'))
+  );
 });
 
 gulp.task(
@@ -82,15 +102,20 @@ gulp.task(
   gulp.series(
     'scss',
     'copy-global-assets',
+    'copy-landing-page-assets',
     'copy-configuration-assets',
     'copy-sandbox-assets',
-    'copy-content-assets'
+    'copy-post-assets'
   )
 );
 
 gulp.task('watch', () => {
   gulp.watch('./src/styles/**/*.scss', gulp.series('scss'));
   gulp.watch('./src/images/**/*', gulp.series('copy-global-assets'));
+  gulp.watch(
+    './src/site/content/en/images/**/*',
+    gulp.series('copy-landing-page-assets')
+  );
   gulp.watch(
     './src/site/content/**/*.{yaml,txt}',
     gulp.series('copy-configuration-assets')
@@ -101,6 +126,6 @@ gulp.task('watch', () => {
   );
   gulp.watch(
     `./src/site/content/en/**/*.{${assetTypes}}`,
-    gulp.series('copy-content-assets')
+    gulp.series('copy-post-assets')
   );
 });
