450c156fd50e1f17645b0e437e5c8ed593f15b49
Rob Dodson <robdodson@google.com>
2019-04-11 13:33:27 -0700

    wip lh audits.

---
+++ src/site/_data/paths/performanceAudits.js
@@ -0,0 +1,22 @@
+module.exports = {
+  url: '/performanceAudits',
+  cover: '/images/collections/lighthouse.png',
+  title: 'Performance audits',
+  description:
+    'How long does your app take to show content and become useable?',
+  overview: `Lorem ipsum dolor set amet...`,
+  topics: [
+    {
+      title: 'Metrics',
+      guides: ['first-contentful-paint'],
+    },
+    {
+      title: 'Opportunities',
+      guides: [],
+    },
+    {
+      title: 'Diagnostics',
+      guides: [],
+    },
+  ],
+};
