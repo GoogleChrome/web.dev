f1c00e925bb13fe3b53531f1a52ed4c66e3e1c36
Rob Dodson <robdodson@google.com>
2019-04-22 20:25:29 -0700

    Strip language dir from hero image.

--- src/site/_includes/components/Hero.js
+++ src/site/_includes/components/Hero.js
@@ -1,10 +1,12 @@
 const {html} = require('common-tags');
+const path = require('path');
+const stripLanguage = require('../../_filters/strip-language');
 
 module.exports = ({page, hero, alt, heroPosition}) => {
   return html`
     <img
       class="w-hero ${heroPosition ? `w-hero--${heroPosition}` : ''}"
-      src="${page.url + hero}"
+      src="${stripLanguage(path.join(page.url, hero))}"
       alt="${alt}"
     />
   `;
