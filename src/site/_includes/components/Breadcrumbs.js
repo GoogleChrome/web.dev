6d7b47e05841d10334ecf238a068b37252f73577
Rob Dodson <robdodson@google.com>
2019-04-23 17:21:47 -0700

    Add missing analytics tags.

--- src/site/_includes/components/Breadcrumbs.js
+++ src/site/_includes/components/Breadcrumbs.js
@@ -2,6 +2,8 @@ const path = require('path');
 const site = require('../../_data/site');
 const {html} = require('common-tags');
 
+/* eslint-disable max-len */
+
 module.exports = (learningPath) => {
   let linkText;
   if (learningPath.slug === 'blog') {
@@ -14,17 +16,25 @@ module.exports = (learningPath) => {
     <ul class="w-breadcrumbs">
       <li class="w-breadcrumbs__crumb">
         <a
-          class="w-breadcrumbs__link w-breadcrumbs__link--left-justify"
+          class="w-breadcrumbs__link w-breadcrumbs__link--left-justify gc-analytics-event"
+          data-category="web.dev"
+          data-label="post, home breadcrumb"
+          data-action="click"
           href="/"
-          >${site.titleVariation}</a
         >
+          ${site.titleVariation}
+        </a>
       </li>
       <li class="w-breadcrumbs__crumb">
         <a
-          class="w-breadcrumbs__link"
+          class="w-breadcrumbs__link gc-analytics-event"
+          data-category="web.dev"
+          data-label="post, path breadcrumb"
+          data-action="click"
           href=${path.join('/', learningPath.slug)}
-          >${linkText}</a
         >
+          ${linkText}
+        </a>
       </li>
     </ul>
   `;
