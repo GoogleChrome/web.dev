5132f292ae31f3d9c5c17094657dc0410f9ef5a3
Michael Friesenhahn <mfriesenhahn@ets.org>
2019-05-03 16:53:51 -0700

    Fix line endings.

--- src/site/content/en/fast/fast.11tydata.js
+++ src/site/content/en/fast/fast.11tydata.js
@@ -9,12 +9,12 @@ module.exports = {
     title: 'Fast load times',
     updated: 'May 24, 2018',
     description: 'Guarantee your site loads quickly to avoid user drop off.',
-    overview: `When you're building a modern web experience, it's important to 
+    overview: `When you're building a modern web experience, it's important to
     measure, optimize, and monitor if you're to get fast and stay fast.
     Performance plays a significant role in the success of any online venture,
     as high performing sites engage and retain users better than poorly
     performing ones.
-    
+
     Sites should focus on optimizing for user-centric happiness metrics. Tools
     like Lighthouse (baked into web.dev!) highlight these metrics and help you
     take the right steps toward improving your performance. To "stay fast", set
