0d8fd7be5fcf9f990dfcbd60f1a74e72789738dd
Bojan Pavic <bojanpavic@google.com>
2019-05-05 12:32:45 +0100

    Add Introduction to Fast

--- src/site/content/en/fast/fast.11tydata.js
+++ src/site/content/en/fast/fast.11tydata.js
@@ -23,8 +23,14 @@ module.exports = {
     your site has launched.`,
     topics: [
       {
-        title: 'Measure your site\'s performance',
-        pathItems: ['discover-performance-opportunities-with-lighthouse'],
+        title: 'Introduction',
+        pathItems: [
+          'why-speed-matters',
+          'what-is-speed',
+          'how-to-measure-speed',
+          'how-to-improve-speed',
+          'how-to-stay-fast',
+        ],
       },
       {
         title: 'Optimize your images',
