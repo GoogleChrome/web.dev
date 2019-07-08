ddfe5f51fde8611d21afd22f97968ec5be8cead3
mgechev <mgechev@gmail.com>
2019-07-08 14:37:38 -0700

    Add the code-splitting article

--- src/site/content/en/angular/angular.11tydata.js
+++ src/site/content/en/angular/angular.11tydata.js
@@ -19,6 +19,12 @@ module.exports = {
           'get-started-optimize-angular',
         ],
       },
+      {
+        title: 'Fast',
+        pathItems: [
+          'route-level-code-splitting',
+        ],
+      },
     ],
   },
 };
