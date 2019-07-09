5d3a1d9636538eeb1381f28c3d46c77b975e0da4
Rob Dodson <robdodson@google.com>
2019-07-09 11:57:19 -0700

    wip add preact

---
+++ src/lib/fb.js
@@ -0,0 +1,28 @@
+// import firebase from 'firebase/app';
+// import 'firebase/auth';
+
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyBjcWKserQhn4mygIluGr9eTMfR_S0PDEU',
+//   authDomain: 'v2-prototype.firebaseapp.com',
+//   databaseURL: 'https://v2-prototype.firebaseio.com',
+//   projectId: 'v2-prototype',
+//   storageBucket: '',
+//   messagingSenderId: '960947587576',
+//   appId: '1:960947587576:web:b8e4ff1671c6c131',
+// };
+
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+
+// const provider = new firebase.auth.GoogleAuthProvider();
+
+// document.addEventListener('signin', function(e) {
+//   firebase.auth().signInWithPopup(provider).then(function(result) {
+//     // The signed-in user info.
+//     const user = result.user;
+//     console.log('success!');
+//     console.log('user', user);
+//   }).catch(function(error) {
+//     console.log('error', error);
+//   });
+// });
