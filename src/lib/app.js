8c280e92de1f8010f6398b99a308df281898acc4
Rob Dodson <robdodson@google.com>
2019-07-10 14:36:14 -0700

    wip

--- src/lib/app.js
+++ src/lib/app.js
@@ -1,6 +1,6 @@
 import '../styles/all.scss';
 import {router} from './router';
-import './components/UserProfile';
+import './components/UserProfileContainer';
 
 // Run as long-lived router w/ history & "<a>" bindings
 // Also immediately calls `run()` handler for current location
--- src/lib/components/UserProfileContainer/index.js
+++ src/lib/components/UserProfileContainer/index.js
@@ -1,13 +1,16 @@
-import {LitElement, html} from 'lit-element';
+import {BaseElement} from '../BaseElement';
+import {html} from 'lit-element';
 import './ProfileSwitcher';
 import './SignIn';
 import {store} from '../../store';
 
 /* eslint-disable require-jsdoc */
-class UserProfile extends LitElement {
+class UserProfileContainer extends BaseElement {
   static get properties() {
     return {
+      checkingSignedInState: {type: Boolean},
       isSignedIn: {type: Boolean},
+      user: {type: Object},
     };
   }
 
@@ -18,9 +21,13 @@ class UserProfile extends LitElement {
   }
 
   render() {
+    if (this.checkingSignedInState) {
+      return '';
+    }
+
     if (this.isSignedIn) {
       return html`
-        <web-profile-switcher></web-profile-switcher>
+        <web-profile-switcher .user="${this.user}"></web-profile-switcher>
       `;
     } else {
       return html`
@@ -31,8 +38,10 @@ class UserProfile extends LitElement {
 
   onStateChanged() {
     const state = store.getState();
+    this.checkingSignedInState = state.checkingSignedInState;
     this.isSignedIn = state.isSignedIn;
+    this.user = state.user;
   }
 }
 
-customElements.define('web-user-profile', UserProfile);
+customElements.define('web-user-profile', UserProfileContainer);
