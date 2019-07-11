121b5d5ebf876cde3e23bc0b2bf7c3ea85e4f116
Rob Dodson <robdodson@google.com>
2019-07-10 18:34:56 -0700

    240px version of profile switcher

--- src/lib/components/UserProfileContainer/ProfileSwitcher.js
+++ src/lib/components/UserProfileContainer/ProfileSwitcher.js
@@ -23,32 +23,73 @@ class ProfileSwitcher extends BaseElement {
     `;
   }
 
+  firstUpdated() {
+    this.expanded = true;
+    // Close the profile switcher if it's open and the user presses escape.
+    this.addEventListener('keyup', (e) => {
+      if (e.key === 'Escape') {
+        if (this.expanded) {
+          this.expanded = false;
+        }
+      }
+    });
+  }
+
   get expandedTemplate() {
+    /**
+     * Note, the Google One Bar will also display a blurb at the top if the
+     * user is signed in with a G Suite account. It does this using the
+     * hosted domain property of the Google Sign-in User object.
+     * https://developers.google.com/identity/sign-in/web/reference#googleusergethosteddomain
+     *
+     * I don't think this data is made available by Firebase auth so we don't
+     * implement this feature.
+     */
     return html`
-      <div>
-        <div>
-          <div>
+      <div class="w-profile-dialog">
+        <div class="w-profile-dialog__user">
+          <div class="w-profile-dialog__photo-container">
             <img
-              style="width: 92px; height: 92px; border-radius: 50%"
+              class="w-profile-dialog__photo"
               src="${this.user.photoURL}"
             >
           </div>
-          <div>
-            <strong>${this.user.displayName}</strong>
-            <div>${this.user.email}</div>
-            <div>
+          <div class="w-profile-dialog__details">
+            <div class="w-profile-dialog__name">
+              ${this.user.displayName}
+            </div>
+            <div class="w-profile-dialog__email">
+              ${this.user.email}
+            </div>
             <a
+              class="w-profile-dialog__privacy"
+              href="https://myaccount.google.com/privacypolicy"
+              target="_blank"
+            >
+              Privacy
+            </a>
+            <a
+              class="w-profile-dialog__account"
               href="https://myaccount.google.com"
               target="_blank"
             >
               Google Account
             </a>
-            </div>
           </div>
         </div>
-        <div>
-          <button @click="${signIn}">Change accounts</button>
-          <button @click="${signOut}">Sign out</button>
+        <div class="w-profile-dialog__controls">
+          <button
+            class="w-profile-dialog__button"
+            @click="${signIn}"
+          >
+            Change accounts
+          </button>
+          <button
+            class="w-profile-dialog__button"
+            @click="${signOut}"
+          >
+            Sign out
+          </button>
         </div>
       </div>
     `;
