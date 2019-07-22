6afe01294cc836dde3e5dc99625486e57f7fa9c0
Rob Dodson <robdodson@google.com>
2019-07-22 08:16:18 -0700

    Cleaning up elements a tad.

--- src/lib/app.js
+++ src/lib/app.js
@@ -1,6 +1,8 @@
-import '../styles/all.scss';
-import {router} from './router';
-import './components/ProfileSwitcherContainer';
+import "../styles/all.scss";
+import {router} from "./router";
+import "./components/ProfileSwitcherContainer";
+import "./components/ProgressBar";
+import "./components/SparklineChart";
 
 // Run as long-lived router w/ history & "<a>" bindings
 // Also immediately calls `run()` handler for current location
