9dfd53c621c2200666de77594a0f656011c7edc3
Sam Thorogood <samthor@users.noreply.github.com>
2019-07-24 00:50:35 +1000

    add global class to page (#1197)

--- src/lib/app.js
+++ src/lib/app.js
@@ -3,7 +3,20 @@ import {router} from "./router";
 import "./components/ProfileSwitcherContainer";
 import "./components/ProgressBar";
 import "./components/SparklineChart";
+import {store} from "./store";
 
 // Run as long-lived router w/ history & "<a>" bindings
 // Also immediately calls `run()` handler for current location
 router.listen();
+
+// Configures global page state
+function onGlobalStateChanged() {
+  const state = store.getState();
+  if (state.isSignedIn) {
+    document.body.classList.add("lh-signedin");
+  } else {
+    document.body.classList.remove("lh-signedin");
+  }
+}
+store.subscribe(onGlobalStateChanged);
+onGlobalStateChanged();
