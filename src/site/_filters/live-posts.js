d9b3a29ef22e5bb2615056c2bf010aed12dbc9ab
Rob Dodson <robdodson@google.com>
2019-07-30 15:44:02 -0700

    Disable scheduled posts.

--- src/site/_filters/live-posts.js
+++ src/site/_filters/live-posts.js
@@ -1,17 +1,16 @@
-const now = new Date();
-
 /**
- * Filter scheduled posts and draft posts out from a collection.
- * This function does produce a side-effect where it adds a draft flag to a
- * post if it's not scheduled to go live. This is so the page template can
- * add a meta noindex tag to the post.
+ * Filter draft posts out from a collection.
  * @param {object} post An eleventy post object.
  * @return {boolean} Whether or not the post should go live.
  */
 module.exports = function livePosts(post) {
-  if (post.date > now) {
-    post.data.draft = true;
-  }
+  // If we ever wanted to enable featured posts we could uncomment these lines.
+  // See https://github.com/GoogleChrome/web.dev/pull/1222#issuecomment-516621561
+  // for an explanation on why this feature is currently disabled.
+  // const now = new Date();
+  // if (post.date > now) {
+  //   post.data.draft = true;
+  // }
 
   return !post.data.draft;
 };
