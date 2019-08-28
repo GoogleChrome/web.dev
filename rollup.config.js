8d2151ed628c966e3ef607f8197927d92425ef3e
Rob Dodson <robdodson@users.noreply.github.com>
2019-08-27 18:44:59 -0700

    Improve build time. (#1443)
    * Improve build time.
    
    * Remove style imports in app.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,5 @@
 import resolve from "rollup-plugin-node-resolve";
 import commonJs from "rollup-plugin-commonjs";
-import postcss from "rollup-plugin-postcss";
-import OMT from "rollup-plugin-off-main-thread";
-import entrypointHashmanifest from "rollup-plugin-entrypoint-hashmanifest";
 
 const isProd = process.env.ELEVENTY_ENV === "prod";
 
@@ -18,7 +15,7 @@ module.exports = [
     input: "src/lib/app.js",
     output: {
       dir: "dist",
-      format: "amd",
+      format: "iife",
       entryFileNames: outputPattern,
       chunkFileNames: outputPattern,
     },
@@ -30,19 +27,13 @@ module.exports = [
       commonJs({
         include: "node_modules/**",
       }),
-      OMT(),
-      postcss({
-        extract: true,
-        sourceMap: isProd ? false : "inline",
-      }),
-      entrypointHashmanifest(),
     ],
   },
   {
     input: "src/lib/test/index.js",
     output: {
       dir: "dist/test",
-      format: "amd",
+      format: "iife",
       entryFileNames: outputPattern,
       chunkFileNames: outputPattern,
     },
@@ -54,7 +45,6 @@ module.exports = [
       commonJs({
         include: "node_modules/**",
       }),
-      OMT(),
     ],
   },
 ];
