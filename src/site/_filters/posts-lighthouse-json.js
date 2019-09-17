90fde531bab3b94c13c46066ffd44aaea387a82d
Rob Dodson <robdodson@google.com>
2019-09-17 12:45:36 -0700

    Allow quotes. Implement our own regex.

--- src/site/_filters/posts-lighthouse-json.js
+++ src/site/_filters/posts-lighthouse-json.js
@@ -15,7 +15,33 @@
  */
 
 const stripLanguage = require('../_filters/strip-language');
-const _ = require('lodash');
+
+const reUnescapedHtml = /[<>]/g;
+const reHasUnescapedHtml = RegExp(reUnescapedHtml.source);
+const htmlEscapes = {
+  '<': '&lt;',
+  '>': '&gt;',
+};
+
+/**
+ * Match a character with its escape counterpart.
+ * @param {string} chr A character
+ * @return {string}
+ */
+function escapeHtmlChar(chr) {
+  return htmlEscapes[chr];
+}
+
+/**
+ * Escape any HTML angle brackets in a string.
+ * @param {string} str A string to escape.
+ * @return {string}
+ */
+function escapeHtml(str) {
+  return str && reHasUnescapedHtml.test(str)
+    ? str.replace(reUnescapedHtml, escapeHtmlChar)
+    : str;
+}
 
 // Generate a JSON object which links posts to their Ligthhouse audits.
 module.exports = (posts) => {
@@ -31,8 +57,7 @@ module.exports = (posts) => {
       topic: '',
       id: post.fileSlug, // e.g. "test-post"
       lighthouse: toArray(post.data.web_lighthouse),
-      // allow posts with html tags in the title.
-      title: _.escape(post.data.title),
+      title: escapeHtml(post.data.title),
       url: stripLanguage(post.url),
     };
 
