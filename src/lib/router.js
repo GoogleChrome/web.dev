f18bd35946c95ad937d65c4c4e86c93a09b625af
Sam Thorogood <samthor@users.noreply.github.com>
2019-09-19 05:19:06 +1000

    basic Analytics updates (#1496)
    * basic Analytics updates
    
    * remove devsite-analytics-scope handler

--- src/lib/router.js
+++ src/lib/router.js
@@ -11,6 +11,9 @@ const domparser = new DOMParser();
  */
 async function getPage(url) {
   const res = await fetch(`/${url}`);
+  if (!res.ok) {
+    throw res.status;
+  }
   const text = await res.text();
   return domparser.parseFromString(text, "text/html");
 }
@@ -21,6 +24,8 @@ async function getPage(url) {
  * @return {Promise}
  */
 async function swapContent(url) {
+  document.dispatchEvent(new CustomEvent("pageview", {detail: url}));
+
   // When the router boots it will always try to run a handler for the current
   // route. But we don't need this for initial page load so we cancel it.
   if (isFirstRun) {
@@ -30,7 +35,15 @@ async function swapContent(url) {
 
   const main = document.querySelector("main");
   // Grab the new page content
-  const page = await getPage(url);
+  let page;
+  try {
+    page = await getPage(url);
+  } catch (e) {
+    // If something fails, just make a browser URL change
+    // TODO(robdodson): In future, failure pages might be HTML themselves
+    window.location.href = window.location.href;
+    throw e;
+  }
   // Remove the current #content element
   main.querySelector("#content").remove();
   // Swap in the new #content element
