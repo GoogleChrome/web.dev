de17e45b5b49438085ba8a1a82c77f56406e8f06
Sam Thorogood <samthor@users.noreply.github.com>
2019-10-01 04:49:36 +1000

    Make content URLs canonical (#1583)
    * resolve pathname ambiguity
    
    * redir instead of 404

--- server.js
+++ server.js
@@ -17,8 +17,33 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('dist'));
-app.use(express.static('dist/en'));
+const contentPageMatch = /^(\/\w+)+/;
+
+// Disallow requests to "index.html" in any subdirectory (send a 302) along with
+// requests to a subdirectory that end with "/". This means that e.g., the
+// measure page, technically at "https://web.dev/measure/index.html", will
+// only be served under "https://web.dev/measure".
+// This resolves ambiguity for Service Workers and the Navaid router (which
+// internally trims trailing slashes for JS-based routing).
+const contentHandler = (req, res, next) => {
+  if (req.url === '/') {
+    req.url = '/index.html';
+  } else if (req.url.endsWith('/index.html') || req.url.endsWith('/')) {
+    const index = req.url.lastIndexOf('/');
+    const redir = req.url.substr(0, index);
+    res.writeHead(302, {Location: redir});
+    return res.end();
+  } else if (contentPageMatch.exec(req.url)) {
+    req.url += '/index.html';
+  }
+  return next();
+};
+
+app.use(
+  express.static('dist', {index: false}),
+  contentHandler,
+  express.static('dist/en', {index: false, redirect: false}),
+);
 
 const listener = app.listen(process.env.PORT || 8080, () => {
   // eslint-disable-next-line
