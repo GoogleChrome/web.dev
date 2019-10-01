380ee3d991b7e24ae2752c76a0be5fc481dd1399
Mark Pieszak <mpieszak84@gmail.com>
2019-10-01 17:54:59 -0400

    post: commit dynamic ssr post

--- src/site/content/en/angular/angular.11tydata.js
+++ src/site/content/en/angular/angular.11tydata.js
@@ -33,7 +33,10 @@ module.exports = {
       },
       {
         title: 'Discoverable',
-        pathItems: ['getting-started-with-server-side-rendering-in-angular'],
+        pathItems: [
+          'getting-started-with-server-side-rendering-in-angular',
+          'setup-dynamic-server-side-rendering-with-angular'
+        ],
       },
       {
         title: 'Installable',
