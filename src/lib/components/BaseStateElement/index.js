c35409f166205367dd57fc90fd15e808c1b681b3
Sam Thorogood <sam.thorogood@gmail.com>
2019-10-21 15:10:13 +1100

    make Measure signin button work

---
+++ src/lib/components/BaseStateElement/index.js
@@ -0,0 +1,30 @@
+import {store} from "../../store";
+import {BaseElement} from "../BaseElement";
+
+/**
+ * Base element which subscribes to global state.
+ *
+ * @extends {BaseElement}
+ */
+export class BaseStateElement extends BaseElement {
+  constructor() {
+    super();
+    this.onStateChanged = this.onStateChanged.bind(this);
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    store.subscribe(this.onStateChanged);
+    this.onStateChanged(store.getState());
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    store.unsubscribe(this.onStateChanged);
+  }
+
+  /**
+   * @param {!Object<string, *>} state
+   */
+  onStateChanged(state) {}
+}
