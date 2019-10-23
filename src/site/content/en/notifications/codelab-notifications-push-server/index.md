04de172b08af2398cee08d84b83eb34be167a548
Kate Jeffreys <katejeffreys@google.com>
2019-10-22 17:39:28 -0700

    Draft notifications collection with nav

---
+++ src/site/content/en/notifications/codelab-notifications-push-server/index.md
@@ -0,0 +1,18 @@
+---
+layout: codelab
+title: codelab-notifications-push-server
+author: katejeffreys
+description: |
+  codelab-notifications-push-server
+date: 2019-10-14
+# Add an updated date to your post if you edit in the future.
+# updated: 2019-06-27
+glitch: codelab-notifications-push-server
+# Specify which file the glitch should start on.
+glitchPath: public/index.js  
+related_post: tbd
+---
+
+## codelab-notifications-push-server
+
+TBD
---
+++ src/site/content/en/notifications/notifications.11tydata.js
@@ -0,0 +1,25 @@
+module.exports = {
+  // Tags are inherited by all posts.
+  tags: ['pathItem', 'notifications'],
+  path: {
+    // Slug is used by landing pages like / and /learn to link to this path.
+    // Because it affects urls, the slug should never be translated.
+    slug: 'notifications',
+    cover: '/images/collections/notifications.svg',
+    title: 'Notifications',
+    updated: 'October 23, 2019',
+    description: `Use push notifications to engage users`,
+    overview: `In this section you'll learn how to create and send notifications. You'll add push capabilities to an app server, and use built-in APIs to subscribe to notifications, receive them from a server, and display them on screen.`,
+    topics: [
+      {
+        title: 'Notifications',
+        pathItems: [
+          'use-push-notifications-to-engage-users',
+          'codelab-notifications-get-started',
+          'codelab-notifications-service-worker',
+          'codelab-notifications-push-server'
+        ],
+      }
+    ],
+  },
+};
