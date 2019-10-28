90bddf4802a32f6f14a7653eb389a669b92f9b08
Sam Thorogood <samthor@users.noreply.github.com>
2019-10-29 10:21:55 +1100

    add fonts and analytics to SW (#1754)
    * add fonts and analytics
    
    * trailing comma

--- src/lib/sw.js
+++ src/lib/sw.js
@@ -4,6 +4,33 @@ importScripts(
   "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js",
 );
 
+workbox.googleAnalytics.initialize();
+
+// Cache the Google Fonts stylesheets with a stale-while-revalidate strategy.
+workbox.routing.registerRoute(
+  /^https:\/\/fonts\.googleapis\.com/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: "google-fonts-stylesheets",
+  }),
+);
+
+// Cache the underlying font files with a cache-first strategy for 1 year.
+workbox.routing.registerRoute(
+  /^https:\/\/fonts\.gstatic\.com/,
+  new workbox.strategies.CacheFirst({
+    cacheName: "google-fonts-webfonts",
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200],
+      }),
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 60 * 60 * 24 * 365,
+        maxEntries: 30,
+      }),
+    ],
+  }),
+);
+
 console.log("Got cache manifest", manifest);
 
 workbox.precaching.precache(["/offline/"]);
