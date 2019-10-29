8a1cc664c0b593b885c72793bc240515fff4e1b8
Sam Thorogood <sam.thorogood@gmail.com>
2019-10-29 15:55:53 +1100

    fix bundles path

--- src/lib/webcomponents-config.js
+++ src/lib/webcomponents-config.js
@@ -6,5 +6,6 @@
  */
 
 // Set the production path for the Web Components polyfills. Must have leading and trailing slash.
+// This actually looks inside the root PLUS "bundles/<filename>".
 // This isn't configurable as we don't need the polyfill in dev.
 window.WebComponents = {root: "/lib/webcomponents/"};
