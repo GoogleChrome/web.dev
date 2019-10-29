8b69238b8cb9d2edaca05f5099ad496e991bbdaf
Paul Irish <paulirish@google.com>
2019-10-29 13:53:01 -0700

    Add Lighthouse CI (#1757)

---
+++ tools/lhci/run-lighthouse-ci.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eox pipefail
+
+export LHCI_TOKEN=d96145f6-c3d2-4b22-9e53-6913cdd2df03
+export LHCI_RC_FILE=./tools/lhci/.lighthouserc
+
+# Install latest LHCI
+npm install -g @lhci/cli@next
+
+# Start up a local webserver and wait for it to get going
+npm start &
+sleep 2
+
+lhci collect
+lhci upload
+
+# Kill the webserver process
+kill $!
