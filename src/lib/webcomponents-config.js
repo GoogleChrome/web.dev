1b5e2210b996a6d159e35f89381409a30cd1736f
Sam Thorogood <samthor@users.noreply.github.com>
2019-10-30 08:19:59 +1100

    More unsupported browser fixes (#1762)
    * simplify wc loader
    
    * show an error on measure if unsupported
    
    * fix slash
    
    * fix bundles path
    
    * don't discard dist/lib
    
    * Edge loading fixes
    
    * copy update

---
+++ src/lib/webcomponents-config.js
@@ -0,0 +1,11 @@
+/**
+ * @fileoverview Configures the WebComponents polyfill path.
+ *
+ * This needs to occur in its own file that is included _before_ the loader itself, because ES6
+ * modules are all hoisted in the order in which they are found.
+ */
+
+// Set the production path for the Web Components polyfills. Must have leading and trailing slash.
+// This actually looks inside the root PLUS "bundles/<filename>".
+// This isn't configurable as we don't need the polyfill in dev.
+window.WebComponents = {root: "/lib/webcomponents/"};
