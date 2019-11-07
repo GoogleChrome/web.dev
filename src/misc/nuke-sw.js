7f38ff6fa1e6f59f98906918f28f7ce02824a635
Sam Thorogood <sam.thorogood@gmail.com>
2019-11-06 16:22:46 -0800

    eslint

--- src/misc/nuke-sw.js
+++ src/misc/nuke-sw.js
@@ -3,19 +3,23 @@
 
 // Is this actually being executed in a ServiceWorker?
 if (self instanceof ServiceWorkerGlobalScope) {
-  Promise.resolve().then(() => {
-    // Nuke the Service Worker.
-    return self.registration.unregister();
-  }).then(() => {
-    // Match all window'ed pages.
-    return clients.matchAll({
-      includeUncontrolled: true,
-      type: "window",
+  Promise.resolve()
+    .then(() => {
+      // Nuke the Service Worker.
+      return self.registration.unregister();
+    })
+    .then(() => {
+      // Match all window'ed pages.
+      return clients.matchAll({
+        includeUncontrolled: true,
+        type: "window",
+      });
+    })
+    .then((clients) => {
+      // Reload all pages (this isn't part of the promise chain, as we can block ourselves).
+      clients.forEach((client) => client.navigate(client.url));
+    })
+    .catch((err) => {
+      console.error(err);
     });
-  }).then((clients) => {
-    // Reload all pages (this isn't part of the promise chain, as we can block ourselves).
-    clients.forEach((client) => client.navigate(client.url));
-  }).catch((err) => {
-    console.error(err);
-  });
 }
