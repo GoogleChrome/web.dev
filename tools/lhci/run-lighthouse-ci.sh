d9a992de597bfcdb0aa37a2c042ec5782dbf6a20
Patrick Hulce <patrick.hulce@gmail.com>
2019-11-11 23:34:21 -0800

    remove token from lhci script

--- tools/lhci/run-lighthouse-ci.sh
+++ tools/lhci/run-lighthouse-ci.sh
@@ -5,4 +5,4 @@ set -eox pipefail
 # Install latest LHCI
 npm install -g @lhci/cli@0.3.x
 
-lhci autorun --config=./tools/lhci/.lighthouserc --upload.token=d96145f6-c3d2-4b22-9e53-6913cdd2df03
+lhci autorun --config=./tools/lhci/.lighthouserc
