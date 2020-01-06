0f6d91b4aad3039308670e8e940a0454427cd51d
Pete LePage <pete.lepage@pobox.com>
2020-01-06 16:01:27 -0500

    What Are PWAs - intro doc

--- src/site/content/en/progressive-web-apps/progressive-web-apps.11tydata.js
+++ src/site/content/en/progressive-web-apps/progressive-web-apps.11tydata.js
@@ -10,12 +10,12 @@ module.exports = {
     title: "Progressive Web Apps",
     updated: "November 1, 2019",
     description: `Websites that took all the right vitamins`,
-    overview: `In this collection, you"ll learn what makes a Progressive Web
+    overview: `In this collection, you'll learn what makes a Progressive Web
     App special, how they can affect your business, and how to build them.`,
     topics: [
       {
         title: "Introduction",
-        pathItems: [],
+        pathItems: ["what-are-pwas"],
       },
       {
         title: "Capable",
