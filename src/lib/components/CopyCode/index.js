c832d65f04dde5e210db6e1fa2d0078ed19cc116
mfriesenhahn <mfriesenhahn@google.com>
2020-02-16 22:04:25 -0800

    Add tooltip to CopyCode

--- src/lib/components/CopyCode/index.js
+++ src/lib/components/CopyCode/index.js
@@ -27,6 +27,13 @@ class CopyCode extends BaseElement {
         "w-button--icon w-button--round web-copy-code__button";
       this.copyButton.setAttribute("data-icon", "file_copy");
       this.copyButton.addEventListener("click", this.onCopy);
+
+      this.tooltip = document.createElement("span");
+      this.tooltip.className = "w-tooltip w-tooltip--right";
+      this.tooltip.setAttribute("role", "tooltip");
+      this.tooltip.innerHTML = "Copy code";
+      this.copyButton.append(this.tooltip);
+
       this.prepend(this.copyButton);
     }
   }
