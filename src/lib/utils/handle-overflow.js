421671e294e75083c912682b26ff5cd1cad1531b
mfriesenhahn <mfriesenhahn@google.com>
2020-02-24 10:01:24 -0800

    Make Tab overflow a prop

--- src/lib/utils/handle-overflow.js
+++ src/lib/utils/handle-overflow.js
@@ -2,16 +2,9 @@
  * Check whether an element has overflow.
  * @param {Object} scroller The element to check for overflow.
  * @param {string} dimension The dimension to check (height or width).
- * @param {string} overflowClass The class to add.
- * @param {Object} container The element to add a class to.
  * @return {null}
  */
-export const handleOverflow = (
-  scroller,
-  dimension,
-  overflowClass,
-  container,
-) => {
+export const handleOverflow = (scroller, dimension) => {
   if (dimension !== "height" && dimension !== "width") {
     return console.error("Can only check overflow for height or width.");
   }
@@ -20,20 +13,10 @@ export const handleOverflow = (
     return console.error("Can't check overflow on an undefined element.");
   }
 
-  if (!overflowClass) {
-    return console.error("No overflow class to apply.");
-  }
-
-  if (!container) container = scroller;
-
   const clientDimension =
     dimension === "width" ? scroller.clientWidth : scroller.clientHeight;
   const scrollDimension =
     dimension === "width" ? scroller.scrollWidth : scroller.scrollHeight;
 
-  if (scrollDimension > clientDimension) {
-    container.classList.add(overflowClass);
-  } else {
-    container.classList.remove(overflowClass);
-  }
+  return scrollDimension > clientDimension;
 };
