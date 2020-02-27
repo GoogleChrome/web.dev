ad4a74610eb6c3ef670bc7370817a7a7554fd586
Sam Thorogood <sam.thorogood@gmail.com>
2020-02-27 11:34:26 +1100

    clear scroll restoration before unload

--- src/lib/utils/router.js
+++ src/lib/utils/router.js
@@ -5,6 +5,11 @@ let recentActiveUrl; // current URL not including hash
 // async, and most browsers will move the scroll position automatically for us, even on old content.
 // Instead, we call `scrollOnFrame` when the async load helper is done.
 window.history.scrollRestoration = "manual";
+window.addEventListener("beforeunload", (e) => {
+  // ... but re-enable when the page is unloaded, which happens if a user refreshes the page using
+  // their browser. This prevents this type of reload from jumping back to the top of the viewport.
+  window.history.scrollRestoration = "auto";
+});
 
 /**
  * @return {string} URL pathname plus optional search part
@@ -156,6 +161,9 @@ export function listen(handler) {
     },
     {passive: true},
   );
+
+  // And on initial load.
+  onDedupScroll();
 
   // Don't catch errors for the first load.
   recentActiveUrl = getUrl();
