3dd27c1b6f95a41f91321a81d1b0b543c2139e2d
Sam Thorogood <sam.thorogood@gmail.com>
2020-03-21 12:12:14 +1100

    test percy nav

---
+++ tools/percy/snapshots-dev.js
@@ -0,0 +1,6 @@
+/**
+ * @fileoverview Invokes snapshots but ensures that headless mode is disabled,
+ * for building new Percy tests.
+ */
+process.env.WEBDEV_PERCY_DEV = "yes"
+require('./snapshots.js');
--- tools/percy/snapshots.js
+++ tools/percy/snapshots.js
@@ -1,4 +1,8 @@
 const PercyScript = require("@percy/script");
+const puppeteer = require('puppeteer');  // for types only
+
+const isDev = Boolean(process.env.WEBDEV_PERCY_DEV)  // set by snapshots-dev.js
+
 const pagesToTest = [
   {
     url: "",
@@ -24,15 +28,22 @@ const pagesToTest = [
 
 // A script to navigate our app and take snapshots with Percy.
 PercyScript.run(
-  async (browser, percySnapshot) => {
-    for (page of pagesToTest) {
-      await browser.goto(`http://localhost:8080/${page.url}`);
+  async (/** @type {!puppeteer.Page} */ page, percySnapshot) => {
+    for (const {url, title} of pagesToTest) {
+      await page.goto(`http://localhost:8080/${url}`);
       // Wait for the SPA to update the active link in the top nav.
-      await browser.waitFor(2000);
-      await percySnapshot(`${page.title}`);
+      await page.waitFor(2000);
+      await percySnapshot(title);
     }
+    // Navigate back to home page by clicking on a link.
+    // This is a canary for problems with the SPA routing code.
+    const navigationPromise = page.waitForNavigation();
+    await page.click(".web-header__logo-link");
+    await navigationPromise;
+    await page.waitFor(200);
+    await percySnapshot("Home page, navigation");
   },
   // These flags remove Percy's single-process flag. Without these, any page
   // with an iframe will crash puppeteer (and percy).
-  {args: ["--no-sandbox", "--disable-setuid-sandbox"]},
+  {headless: !isDev, args: ["--no-sandbox", "--disable-setuid-sandbox"]},
 );
