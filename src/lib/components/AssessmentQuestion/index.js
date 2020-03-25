fb8e7594c252ca914acb00dbc0cd957e01197565
Michael Friesenhahn <mfriesenhahn@google.com>
2020-03-24 21:08:54 -0700

    Remove refs to feedback form

--- src/lib/components/AssessmentQuestion/index.js
+++ src/lib/components/AssessmentQuestion/index.js
@@ -23,15 +23,8 @@ class AssessmentQuestion extends BaseElement {
     this.ctaLabel = "Check";
     this.idSalt = BaseElement.generateIdSalt("web-question-");
 
-    // this.updateResponseComponents = this.updateResponseComponents.bind(this);
-    // this.checkNextQuestion = this.checkNextQuestion.bind(this);
-    // this.requestNextQuestionNav = this.requestNextQuestionNav.bind(this);
-    // this.requestAssessmentReset = this.requestAssessmentReset.bind(this);
     this.responseComponentUpdated = this.responseComponentUpdated.bind(this);
     this.reset = this.reset.bind(this);
-    // this.toggleFooterButtons = this.toggleFooterButtons.bind(this);
-    this.onFeedbackModalClosed = this.onFeedbackModalClosed.bind(this);
-    this.onFeedbackDrawerClosed = this.onFeedbackDrawerClosed.bind(this);
   }
 
   render() {
@@ -51,22 +44,7 @@ class AssessmentQuestion extends BaseElement {
         ${this.prerenderedChildren}
       </div>
       <div class="web-question__footer">
-        <button
-          @click="${this.onFeedbackModalOpen}"
-          class="w-button web-assessment__button web-assessment-feedback-container__open-modal gc-analytics-event"
-          data-category="Self-assessments"
-          data-label="feedbackOpen, web-question-${this.idSalt}"
-        >
-          Report issue
-        </button>
-        <button
-          @click="${this.onFeedbackDrawerOpen}"
-          class="w-button web-assessment__button web-assessment-feedback-container__open-drawer gc-analytics-event"
-          data-category="Self-assessments"
-          data-label="feedbackOpen, web-question-${this.idSalt}"
-        >
-          Report issue
-        </button>
+        <span></span>
         <button
           @click="${this.onSubmit}"
           class="w-button w-button--primary web-assessment__button web-question__cta gc-analytics-event"
@@ -76,11 +54,6 @@ class AssessmentQuestion extends BaseElement {
         >
           ${this.ctaLabel}
         </button>
-        <web-assessment-feedback-container
-          class="web-modal"
-          aria-label="Question feedback form"
-          parent-modal="web-assessment"
-        ></web-assessment-feedback-container>
       </div>
     `;
     /* eslint-enable indent */
@@ -96,12 +69,7 @@ class AssessmentQuestion extends BaseElement {
       );
     }
 
-    // Listen for close event from child feedback form to reenable footer buttons
-    const feedbackForm = this.querySelector(
-      "web-assessment-feedback-container",
     );
-    feedbackForm.addEventListener("close-modal", this.onFeedbackModalClosed);
-    feedbackForm.addEventListener("close-drawer", this.onFeedbackDrawerClosed);
   }
 
   // Update question state based on state of response components.
@@ -144,54 +112,6 @@ class AssessmentQuestion extends BaseElement {
     }
   }
 
-  // Open question feedback form
-  onFeedbackModalOpen(e) {
-    const feedbackForm = this.querySelector(
-      "web-assessment-feedback-container",
-    );
-
-    this.toggleFooterButtons(true);
-    feedbackForm.open = true;
-  }
-
-  // Re-enable question footer buttons when feedback form is closed.
-  onFeedbackModalClosed() {
-    const openFormButton = this.querySelector(
-      ".web-assessment-feedback-container__open-modal",
-    );
-
-    this.toggleFooterButtons(false);
-    openFormButton.focus();
-  }
-
-  onFeedbackDrawerOpen(e) {
-    const feedbackForm = this.querySelector(
-      "web-assessment-feedback-container",
-    );
-
-    this.toggleFooterButtons(true);
-    feedbackForm.openDrawer = true;
-  }
-
-  onFeedbackDrawerClosed() {
-    const openFormButton = this.querySelector(
-      ".web-assessment-feedback-container__open-drawer",
-    );
-
-    this.toggleFooterButtons(false);
-    openFormButton.focus();
-  }
-
-  toggleFooterButtons(state) {
-    const footerButtons = this.querySelectorAll(
-      ".web-question__footer > button",
-    );
-
-    for (const button of footerButtons) {
-      button.disabled = state;
-    }
-  }
-
   updateResponseComponents() {
     const responseComponents = this.querySelectorAll("[data-role=response]");
 
@@ -200,7 +120,7 @@ class AssessmentQuestion extends BaseElement {
     }
   }
 
-  // This should probably emit a custom event that the Tabs component responds to.
+  // This should maybe emit a custom event that the Tabs component responds to?
   checkNextQuestion() {
     const panel = this.closest(".web-tabs__panel");
 
