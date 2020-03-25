2d975d3df8285e27e6c1ec68edeabe3d0ebb8a38
Sam Thorogood <sam.thorogood@gmail.com>
2020-03-25 16:45:33 +1100

    fix bad merge

--- tools/percy/snapshots-dev.js
+++ tools/percy/snapshots-dev.js
@@ -2,5 +2,5 @@
  * @fileoverview Invokes snapshots but ensures that headless mode is disabled,
  * for building new Percy tests.
  */
-process.env.WEBDEV_PERCY_DEV = "yes"
-require('./snapshots.js');
+process.env.WEBDEV_PERCY_DEV = "yes";
+require("./snapshots.js");
