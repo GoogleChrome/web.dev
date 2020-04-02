c33cbab4b9cbfc1ac31c2197274933e589d3bc94
Rob Dodson <robdodson@google.com>
2020-04-02 16:39:55 -0700

    Make integration test run build.

--- test/integration/build-test.js
+++ test/integration/build-test.js
@@ -1,10 +1,24 @@
 const assert = require("assert");
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
 const fs = require("fs");
 const path = require("path");
 const dist = path.resolve(__dirname, "..", "..", "dist");
 
 describe("Build", function() {
-  it("generates the expected files", function() {
+  it("generates the expected files", async function() {
+    // Disable the timeout as it'll take build a little while to finish.
+    // eslint-disable-next-line
+    this.timeout(0);
+
+    console.log("Running npm run build...");
+    try {
+      await exec("npm run build");
+    } catch (err) {
+      assert.fail(err);
+    }
+    console.log("Build completed. Starting tests.");
+
     [
       path.join("en", "_redirects.yaml"),
       path.join("en", "algolia.json"),
