31d080a8427d0228e757e6649f8e2fae6d5e7733
Rob Dodson <robdodson@google.com>
2020-04-10 14:27:30 -0700

    Fix scss. Break js.

--- src/lib/actions.js
+++ src/lib/actions.js
@@ -2,6 +2,8 @@ import {store} from "./store";
 import {saveUserUrl} from "./fb";
 import {runLighthouse, fetchReports} from "./lighthouse-service";
 
+const foo = 'bar';
+foo = 'baz';
 export const clearSignedInState = store.action(() => {
   const {isSignedIn} = store.getState();
   if (isSignedIn) {
