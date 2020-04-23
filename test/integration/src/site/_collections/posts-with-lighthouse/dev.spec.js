18e952359b66e52a6a1f86cbe25a4f64966b9bf1
Ewa Gasperowicz <devnook@gmail.com>
2020-04-23 16:48:03 +0200

    improve comment

--- test/integration/src/site/_collections/posts-with-lighthouse/dev.spec.js
+++ test/integration/src/site/_collections/posts-with-lighthouse/dev.spec.js
@@ -12,7 +12,7 @@ const outputPath = path.join(
 );
 
 describe('posts-with-lighthouse', function() {
-  it('creates postsWithLighthouse collection in dev env', async function() {
+  it('includes posts with a pathItem tag and a web_lighthouse property', async function() {
     const expected = '<p>test-3</p><p>test-4</p>';
     const actual = fs.readFileSync(outputPath, 'utf8');
     assert.equal(actual, expected);
