d64d4297ea5a169a29c36f8a74c76441dbafc00e
Rob Dodson <robdodson@google.com>
2020-04-30 13:35:01 -0700

    Update to lastest actions.

--- .github/actions/lint-front-matter/utils/get-markdown-files.js
+++
@@ -1,9 +0,0 @@
-const isMarkdownFile = new RegExp(/^.*\.(md|markdown)$/);
-
-/**
- * @param {string} files A space separated list of file names.
- * @return {Array<string>} An array of markdown file names.
- */
-module.exports = (files = '') => {
-  return files.split(' ').filter((file) => isMarkdownFile.test(file));
-};
