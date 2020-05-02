102b0298dae948ea6312a9d8fca3c7c39d6e9707
Michael Solati <mkslt04@gmail.com>
2020-05-02 15:43:12 -0700

    fix(newsletter): ensure `<a>` in newsletter pages open in window, not `<iframe>`

--- src/lib/components/Newsletter/index.js
+++ src/lib/components/Newsletter/index.js
@@ -21,6 +21,9 @@ class Newsletter extends BaseElement {
     super.connectedCallback();
     this.iframe = this.querySelector('iframe');
     this.iframe.scrolling = 'no';
+    this.iframe.contentWindow.document.querySelectorAll('a').forEach((a) => {
+      a.target = a.target === '_blank' ? a.target : '_parent';
+    });
     this.resizeIFrame();
     window.addEventListener('resize', this.resizeIFrame);
   }
