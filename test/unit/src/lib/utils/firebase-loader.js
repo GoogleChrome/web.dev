5c5436d0eb5792299792a9e6f86f75899d2cfd43
Sam Thorogood <sam.thorogood@gmail.com>
2020-05-05 16:25:40 +1000

    simplify test

--- test/unit/src/lib/utils/firebase-loader.js
+++ test/unit/src/lib/utils/firebase-loader.js
@@ -1,6 +1,9 @@
 const {assert} = require('../assert');
 const loadFirebase = require('../../../../../src/lib/utils/firebase-loader');
 
+// TODO(samthor): This test doesn't clear the internal cache between tests, as
+// it uses a singleton loading cache. Add a reset for test method.
+
 describe('firebase-loader', function() {
   describe('loader', function() {
     it('should return a Promise', async function() {
@@ -14,39 +17,24 @@ describe('firebase-loader', function() {
       assert(node === null, 'no firebase loads requested');
     });
 
-    it('should not add multiple nodes', async function() {
-      loadFirebase('app');
-      let nodes = [];
-
-      try {
-        nodes = document.head.querySelectorAll(
-          'script[src^="//www.gstatic.com/firebasejs/"]',
-        );
-        assert(nodes.length === 1);
-
-        loadFirebase('app', 'performance');
-        nodes = document.head.querySelectorAll(
-          'script[src^="//www.gstatic.com/firebasejs/"]',
-        );
-        assert(nodes.length === 2, 'nodes should only contain one additional');
-      } finally {
-        // This isn't an async test, we don't check that the scripts actually
-        // load, so just remove them immediately after run.
-        nodes.forEach((node) => node.remove());
-      }
-    });
-
     it('should add script tags after invocation', function() {
       loadFirebase('app', 'performance', 'app');
-      const nodes = document.head.querySelectorAll(
-        'script[src^="//www.gstatic.com/firebasejs/"]',
-      );
+      let nodes = [];
       try {
+        nodes = document.head.querySelectorAll(
+          'script[src^="//www.gstatic.com/firebasejs/"]',
+        );
         assert(nodes.length === 2, 'JS should be added for uniques');
         assert(nodes[0].src.endsWith('-app.js'));
         assert(!nodes[0].async);
         assert(nodes[1].src.endsWith('-performance.js'));
         assert(!nodes[1].async);
+
+        loadFirebase('app');
+        nodes = document.head.querySelectorAll(
+          'script[src^="//www.gstatic.com/firebasejs/"]',
+        );
+        assert(nodes.length === 2, 'JS should remain the same');
       } finally {
         // This isn't an async test, we don't check that the scripts actually
         // load, so just remove them immediately after run.
