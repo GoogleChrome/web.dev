c3f5d4522856b6c3896ce4884de45ff0c338c243
Joseph Medley <jmedley@google.com>
2020-05-13 07:22:26 -0700

    Change double to single quotes.

--- src/site/content/en/learn/learn.11tydata.js
+++ src/site/content/en/learn/learn.11tydata.js
@@ -7,8 +7,8 @@ const discoverable = require('../discoverable/discoverable.11tydata.js').path;
 const secure = require('../secure/secure.11tydata.js').path;
 const pwa = require('../progressive-web-apps/progressive-web-apps.11tydata.js')
   .path;
-const media = require("../media/media.11tydata.js").path;
-const metrics = require("../metrics/metrics.11tydata.js").path;
+const media = require('../media/media.11tydata.js').path;
+const metrics = require('../metrics/metrics.11tydata.js').path;
 const vitals = require('../vitals/vitals.11tydata.js').path;
 
 const react = require('../react/react.11tydata.js').path;
