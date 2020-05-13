06a6b697791330d70b6e8fa2db61b9229fb507d8
Joseph Medley <jmedley@google.com>
2020-05-13 07:22:26 -0700

    Change double to single quotes.

--- src/site/content/en/media/media.11tydata.js
+++ src/site/content/en/media/media.11tydata.js
@@ -1,59 +1,59 @@
 module.exports = {
   // Tags are inherited by all posts.
-  tags: ["media"],
+  tags: ['media'],
   path: {
     // Slug is used by landing pages like / and /learn to link to this path.
     // Because it affects urls, the slug should never be translated.
-    slug: "media",
-    cover: "/images/collections/media.svg",
-    title: "Media",
-    updated: "April 30, 2020",
+    slug: 'media',
+    cover: '/images/collections/media.svg',
+    title: 'Media',
+    updated: 'April 30, 2020',
     description: `Add audio and video to your web sites`,
     overview: `Learn to add media capabilities to your website using either
     basic markdown or a media framework such as Google's Shaka Player, JW
     Player or Video.js.`,
     topics: [
       {
-        title: "Conceptual basics",
+        title: 'Conceptual basics',
         pathItems: [
-          "media-experience",
-          "file-basics",
-          "application-basics",
-          "cheetsheet",
+          'media-experience',
+          'file-basics',
+          'application-basics',
+          'cheetsheet',
         ],
       },
       {
-        title: "Prepare media files for the web",
+        title: 'Prepare media files for the web',
         pathItems: [
-          "for-the-web",
-          "containers-and-codecs",
-          "bitrate",
-          "resolution",
-          "encryption",
-          "application-cheetsheet",
+          'for-the-web',
+          'containers-and-codecs',
+          'bitrate',
+          'resolution',
+          'encryption',
+          'application-cheetsheet',
         ],
       },
       {
-        title: "Prepare the web page",
+        title: 'Prepare the web page',
         pathItems: [
-          "the-video-tag",
-          "the-audio-tag",
-          "the-source-tag",
-          "the-track-tag",
-          "media-controls",
+          'the-video-tag',
+          'the-audio-tag',
+          'the-source-tag',
+          'the-track-tag',
+          'media-controls',
         ],
       },
       {
-        title: "Use media frameworks",
+        title: 'Use media frameworks',
         pathItems: [
-          "for-frameworks",
-          "splitting-streams",
-          "selecting-a-framework",
+          'for-frameworks',
+          'splitting-streams',
+          'selecting-a-framework',
         ],
       },
       {
-        title: "The media server",
-        pathItems: ["preloading", "server-basics"],
+        title: 'The media server',
+        pathItems: ['preloading', 'server-basics'],
       },
     ],
   },
