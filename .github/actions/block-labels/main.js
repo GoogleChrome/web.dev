d963361ec5af59add999accc273c4127175008a1
Rob Dodson <robdodson@google.com>
2020-05-18 12:44:01 -0700

    wip

--- .github/actions/block-labels/main.js
+++ .github/actions/block-labels/main.js
@@ -1,3 +1,14 @@
-async function run() {}
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+function run() {
+  const prLabels = github.context.payload.pull_request.labels;
+  const labelsToBlock = core.getInput('labels');
+  for (const label of labelsToBlock) {
+    if (prLabels.includes(label)) {
+      core.setFailed();
+    }
+  }
+}
 
 run();
