d2300e101d39eaeed2b46d4353fa92bbfa48debe
Sam Thorogood <samthor@users.noreply.github.com>
2020-05-19 11:53:02 +1000

    web.dev/live content update (#2905)
    * fix bad images
    
    * add event data
    
    * add speaker images

--- src/site/_data/event.js
+++ src/site/_data/event.js
@@ -26,11 +26,43 @@ module.exports = [
     sessions: [
       {
         speaker: 'addyosmani',
-        title: 'How to optimize for Web Vitals',
+        title: 'Optimize User Experience for Core Web Vitals',
       },
       {
         speaker: 'rviscomi',
-        title: 'How to browse CrUX data on BigQuery',
+        title: 'Mastering the Chrome UX Report on BigQuery',
+      },
+      {
+        speaker: 'houssein',
+        title: 'How to Analyze Your JavaScript Bundles',
+      },
+      {
+        speaker: 'egsweeny',
+        title: "What's New in Speed Tooling",
+      },
+      {
+        speaker: 'paullewis',
+        title: 'Core Web Vitals in the DevTools Timeline',
+      },
+      {
+        speaker: 'patrickkettner',
+        title: 'AMPs performance secrets',
+      },
+      {
+        speaker: 'sebabenz', // TODO(samthor): add nainar
+        title: 'AMP at your Service',
+      },
+      {
+        speaker: 'morss',
+        title: 'Workerized JavaScript Made Easy',
+      },
+      {
+        speaker: 'martinsplitt',
+        title: 'Debugging SEO problems in JavaScript web apps',
+      },
+      {
+        speaker: 'martinsplitt',
+        title: 'Implementing Structured Data with JavaScript',
       },
     ],
   },
@@ -40,8 +72,58 @@ module.exports = [
     duration: 8,
     sessions: [
       {
+        speaker: 'paullewis',
+        title: "What's new in DevTools",
+      },
+      {
+        speaker: 'syg', // TODO(samthor): add leszeks
+        title: "What's new in V8/JavaScript",
+      },
+      {
+        speaker: 'mathiasbynens',
+        title: "What's new in Puppeteer",
+      },
+      {
+        speaker: 'andreban',
+        title: 'Shipping a PWA as an Android app',
+      },
+      {
+        speaker: 'demianrenzulli',
+        title: 'How to define your install strategy',
+      },
+      {
+        speaker: 'beaufortfrancois',
+        title: 'Taking your media experience to the next level',
+      },
+      {
+        speaker: 'thomassteiner',
+        title:
+          "Building for Modern Browsers and Progressively Enhancing Like It's 2003",
+      },
+      {
+        speaker: 'demianrenzulli',
+        title: 'Advanced PWA Patterns',
+      },
+      {
+        speaker: 'pjmclachlan',
+        title: 'Giving your PWA superpowers 🦹‍♀️',
+      },
+      {
         speaker: 'samthor',
         title: 'App-Like UX for Better PWAs',
+      },
+      {
+        speaker: 'pjmclachlan',
+        title: 'Taming web notifications',
+      },
+      {
+        speaker: 'petelepage',
+        title: 'Storage for the Web',
+      },
+      {
+        speaker: 'nattestad',
+        title:
+          'Building Zoom on the Web: Advanced APIs for high performance web apps',
       },
     ],
   },
@@ -49,6 +131,55 @@ module.exports = [
     title: 'Day 3',
     when: '2020-07-02T16:00Z',
     duration: 8,
-    sessions: [],
+    sessions: [
+      {
+        speaker: 'kosamari',
+        title: 'Building better in the world of build tools!',
+      },
+      {
+        speaker: 'una',
+        title: '10 modern layouts in 1 line of CSS',
+      },
+      {
+        speaker: 'jakearchibald', // TODO(samthor): add jasonjmiller
+        title: 'Writing rollup plugins',
+      },
+      {
+        speaker: 'jakearchibald',
+        title: 'Image compression formats compared',
+      },
+      {
+        speaker: 'sfluin',
+        title: 'Stay Fast and Fresh with Angular',
+      },
+      {
+        speaker: 'samdutton',
+        title: 'Digging into the Privacy Sandbox',
+      },
+      {
+        speaker: 'rowan_m',
+        title: 'Cookie recipes - SameSite and beyond',
+      },
+      {
+        speaker: 'agektmr',
+        title: 'Isolate your origin using COOP+COEP',
+      },
+      {
+        speaker: 'samdutton',
+        title: 'Find and fix problems with the DevTools Issues Panel',
+      },
+      {
+        speaker: 'maudn', // TODO(samthor): add rowan_m
+        title: 'Get a (User-Agent Client) Hint',
+      },
+      {
+        speaker: 'samdutton',
+        title: 'Signin form best practice',
+      },
+      {
+        speaker: 'agektmr',
+        title: "What's new in Web Payments",
+      },
+    ],
   },
 ];
