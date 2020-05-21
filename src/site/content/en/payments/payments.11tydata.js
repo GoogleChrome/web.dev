17dfa47f19f96bc6a6367942eabb163d553cafe1
Milica <mihajlija@gmail.com>
2020-05-22 00:41:34 +0200

    Add the Web Payments collection to /learn (#2929)
    * Add the Web Payments collection

---
+++ src/site/content/en/payments/payments.11tydata.js
@@ -0,0 +1,20 @@
+module.exports = {
+  // Tags are inherited by all posts.
+  tags: ['pathItem', 'payments'],
+  path: {
+    // Slug is used by landing pages like / and /learn to link to this path.
+    // Because it affects urls, the slug should never be translated.
+    slug: 'payments',
+    cover: '/images/collections/payments.svg',
+    title: 'Web Payments',
+    updated: 'May 25, 2020',
+    description: `Easy way to implement payments on the web.`,
+    overview: `In this section you'll learn how to use Web Payments APIs.`,
+    topics: [
+      {
+        title: 'Web Payments',
+        pathItems: ['setting-up-a-payment-method'],
+      },
+    ],
+  },
+};
