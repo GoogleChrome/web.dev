f60b4115d0a5e2dec79dda40d487d6e222dd30a2
Michael Solati <mkslt04@gmail.com>
2020-06-02 10:19:51 -0700

    feat(log-errors): log errors from subscribe form

--- src/lib/components/Subscribe/index.js
+++ src/lib/components/Subscribe/index.js
@@ -3,7 +3,7 @@
  */
 
 import {BaseElement} from '../BaseElement';
-import {trackEvent} from '../../analytics';
+import {trackError, trackEvent} from '../../analytics';
 import './_styles.scss';
 
 /**
@@ -65,15 +65,23 @@ class Subscribe extends BaseElement {
     }).then((r) => r.json());
   }
 
-  onError(errors) {
+  /**
+   *
+   * @param {Error} [error]
+   * @param {boolean} [useDefault]
+   */
+  onError(error, useDefault = false) {
+    const pTag = document.createElement('p');
+    const defaultError = new Error('Could not submit, please try again.');
     this.subscribeError.textContent = '';
-    if (errors) {
-      Object.values(errors).forEach((e) => {
-        const pTag = document.createElement('p');
-        pTag.textContent = typeof e === 'string' ? e : e.join(' ');
-        this.subscribeError.appendChild(pTag);
-      });
-    }
+
+    pTag.textContent = useDefault
+      ? defaultError.message
+      : (error || defaultError).message;
+
+    this.subscribeError.appendChild(pTag);
+
+    trackError(error, 'Email form failed to submit because');
   }
 
   onSubmit(e) {
@@ -96,12 +104,13 @@ class Subscribe extends BaseElement {
         if (response && response.result === 'accepted') {
           this.onSuccess();
         } else if (response && response.errors) {
-          this.onError(response.errors);
+          const errorMessage = Object.values(error).join(' ');
+          this.onError(new Error(errorMessage));
         } else {
-          this.onError({any: ['Could not submit, please try again.']});
+          this.onError(new Error(response.result), true);
         }
       })
-      .catch(() => this.onError({any: ['Could not submit, please try again.']}))
+      .catch((e) => this.onError(e, true))
       .finally(() => (this.processing = false));
   }
 
