b0ac95b8a698b8a0ed3fe41a23572565dc551a0b
Michael Solati <mkslt04@gmail.com>
2020-06-04 10:51:47 -0700

    refactor(generate-salt): import `BaseElement`

--- src/lib/utils/generate-salt.js
+++ src/lib/utils/generate-salt.js
@@ -1,6 +1,6 @@
+import {BaseElement} from '../../lib/components/BaseElement';
+
 /**
- * @TODO Where does BaseElement come from?
- *
  * Generates a random string that can be used to ensure uniqueness of
  * the elements id on a page.
  * @param {string} idPrefix An id prefix to be followed by the generated salt.
@@ -10,6 +10,6 @@
 export const generateIdSalt = (idPrefix) => {
   const salt = Math.random().toString(36).substr(2, 9);
   return document.getElementById(idPrefix + salt)
-    ? BaseElement.generateIdSalt(idPrefix) // eslint-disable-line
+    ? BaseElement.generateIdSalt(idPrefix)
     : salt;
 };
