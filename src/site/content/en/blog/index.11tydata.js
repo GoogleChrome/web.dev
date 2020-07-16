8c54e278e1d08d2dad7d0befdac3f07d7752e8c4
Ewa Gasperowicz <devnook@gmail.com>
2020-07-16 22:53:19 +0200

    Filter blod collection by language

--- src/site/content/en/blog/index.11tydata.js
+++ src/site/content/en/blog/index.11tydata.js
@@ -15,9 +15,10 @@
  */
 
 const {index} = require('../../../_collections/hooks/blog');
+const lang = require('../lang.11tydata');
 
 module.exports = {
   pagination: {
-    before: (blogPosts) => index(blogPosts),
+    before: (blogPosts) => index(blogPosts, lang.lang),
   },
 };
