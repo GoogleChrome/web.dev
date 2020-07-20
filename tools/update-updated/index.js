a2022aab9b28814b604dd7859d7ed5f98ba90194
Rob Dodson <robdodson@google.com>
2020-07-20 15:11:23 -0700

    Disable updated hook.

--- tools/update-updated/index.js
+++ tools/update-updated/index.js
@@ -1,5 +1,9 @@
 /**
  * @fileoverview Task for updating `updated` YAML field.
+ * Run this command while you have dirty files and it will update their
+ * `updated` YAML field.
+ *
+ * Example: npm run updated && git commit -am 'Updated some blog posts'
  *
  * @author Matt Gaunt & Rob Dodson 💕
  */
