4bfd6bd0c4a499f8f57b7445f8bf8021cafbf85f
Michael Solati <mkslt04@gmail.com>
2020-09-21 18:21:19 -0700

    refactor(types): add types

--- types/modules/webdev_entrypoint/index.d.ts
+++ types/modules/webdev_entrypoint/index.d.ts
@@ -14,23 +14,7 @@
  * limitations under the License.
  */
 
-const slugify = require('slugify');
-
-/**
- * Slugifies string.
- *
- * @param {string|StringConstructor} str
- * @return {string}
- */
-module.exports = (str) => {
-  if (str instanceof String) {
-    str = str.toString();
-  } else if (typeof str !== 'string') {
-    return '';
-  }
-
-  return slugify(str, {
-    replacement: '-',
-    lower: true,
-  });
-};
+declare module 'webdev_entrypoint' {
+  const value: string;
+  export default value;
+}
