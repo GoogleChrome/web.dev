2dfe263cb17ab3ccf30d0a7abfb1d638903244b1
Rob Dodson <robdodson@google.com>
2020-09-22 09:15:03 -0700

    Add removal of _exile

--- tools/isolate/index.js
+++ tools/isolate/index.js
@@ -2,25 +2,27 @@ const util = require('util');
 const path = require('path');
 const glob = util.promisify(require('glob'));
 const mv = require('move-file');
+const fs = require('fs').promises;
 
 async function isolate(ignore = []) {
   const matches = await glob('src/site/content/**/*.md', {ignore});
-  matches.forEach(async (oldPath) => {
+  for (const oldPath of matches) {
     let newPath = oldPath.split(path.sep);
     newPath.splice(2, 1, '_exile');
     newPath = newPath.join(path.sep);
     await mv(oldPath, newPath);
-  });
+  }
 }
 
 async function integrate() {
   const matches = await glob('src/site/_exile/**/*.md');
-  matches.forEach(async (oldPath) => {
+  for (const oldPath of matches) {
     let newPath = oldPath.split(path.sep);
     newPath.splice(2, 1, 'content');
     newPath = newPath.join(path.sep);
     await mv(oldPath, newPath);
-  });
+  }
+  await fs.rmdir('src/site/_exile', {recursive: true});
 }
 
 const command = process.argv[2];
