939d0c1304d773c583491498e33d7a21e612530e
demianrenzulli <demian77@gmail.com>
2020-12-04 16:38:36 -0500

    Two way communication guide.

--- src/site/content/en/reliable/imperative-caching-guide/samples.js
+++
@@ -1,9 +0,0 @@
-/*
-{
-    productName : "banana",
-    productPic : "https://cdn.example.com/product_images/banana.jpeg",
-    unitPrice : "1.99"
-  }
-*/
-
-{ productName: "banana", productPic: "https://cdn.example.com/product_images/banana.jpeg", unitPrice: "1.99"}
