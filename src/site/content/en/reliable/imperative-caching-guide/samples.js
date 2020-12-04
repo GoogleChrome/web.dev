4f4c57c15acbb34e9fc8f79419e8746a5cdc45ee
demianrenzulli <demian77@gmail.com>
2020-12-04 12:33:56 -0500

    Imperative caching guide.

---
+++ src/site/content/en/reliable/imperative-caching-guide/samples.js
@@ -0,0 +1,9 @@
+/*
+{
+    productName : "banana",
+    productPic : "https://cdn.example.com/product_images/banana.jpeg",
+    unitPrice : "1.99"
+  }
+*/
+
+{ productName: "banana", productPic: "https://cdn.example.com/product_images/banana.jpeg", unitPrice: "1.99"}
