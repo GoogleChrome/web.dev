f4b77e22d0fdfe7e43d587114bf7348a101b38ae
tomayac <steiner.thomas@gmail.com>
2021-02-02 16:35:10 +0100

    Prettier

--- src/lib/urls.js
+++ src/lib/urls.js
@@ -35,26 +35,28 @@ export function copyLinkToClipboard() {
   if (!('clipboard' in navigator)) {
     return;
   }
-  document.querySelector('main')
-  // ToDo: Use `:is(h1, h2, h3, h4, h5, h6)[id]` once support is better.
-  .querySelectorAll('h1[id], h2[id], h3[id], h4[id], h5[id], h6[id]')
-  .forEach(heading => {
-    heading.addEventListener('click', (ev) => {
-      // Don't jump when the '#' is clicked.
-      if (ev.target.nodeName === 'A') {
-        ev.preventDefault();
-      }
-      try {
-        navigator.clipboard.writeText(
-            `${location.origin}${location.pathname}#${heading.id}`);
-        const temp = heading.innerHTML;
-        heading.innerHTML += '&nbsp;<small>(📋 Copied.)</small>';
-        setTimeout(() => {
-          heading.innerHTML = temp;
-        }, 2000);
-      } catch(err) {
-        console.warn(err.name, err.message);
-      }
+  document
+    .querySelector('main')
+    // ToDo: Use `:is(h1, h2, h3, h4, h5, h6)[id]` once support is better.
+    .querySelectorAll('h1[id], h2[id], h3[id], h4[id], h5[id], h6[id]')
+    .forEach((heading) => {
+      heading.addEventListener('click', (ev) => {
+        // Don't jump when the '#' is clicked.
+        if (ev.target.nodeName === 'A') {
+          ev.preventDefault();
+        }
+        try {
+          navigator.clipboard.writeText(
+            `${location.origin}${location.pathname}#${heading.id}`,
+          );
+          const temp = heading.innerHTML;
+          heading.innerHTML += '&nbsp;<small>(📋 Copied.)</small>';
+          setTimeout(() => {
+            heading.innerHTML = temp;
+          }, 2000);
+        } catch (err) {
+          console.warn(err.name, err.message);
+        }
+      });
     });
-  });
 }
