4c627f6fd3ba51ee8edb700a4c7dd775b0297e29
Rob Dodson <robdodson@google.com>
2021-02-09 17:41:02 -0800

    Clean up and fix failing test.

--- test/unit/src/lib/index.js
+++ test/unit/src/lib/index.js
@@ -1,8 +1,5 @@
 require('./urls');
-// FIXME: In the new setup this always fails with:
-// TypeError: loadFirebase$1 is not a function
-// I think it's a rollup issue but I suck at debugging wack bundler shiz. halp.
-// require('./utils/firebase-loader');
+require('./utils/firebase-loader');
 require('./components/LighthouseGauge');
 require('./components/LivestreamContainer');
 require('./components/ProgressBar');
