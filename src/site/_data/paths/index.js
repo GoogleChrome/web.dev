fdc4993d1e19e0598ed5f73c2d3d17c974b9f3f8
Milica Mihajlija <mihajlija@gmail.com>
2021-02-10 12:53:55 +0100

    Add identity collection.

--- src/site/_data/paths/index.js
+++ src/site/_data/paths/index.js
@@ -18,6 +18,8 @@ const react = require('./react.json');
 const reliable = require('./reliable.json');
 const secure = require('./secure.json');
 const animations = require('./animations.json');
+const identity = require('./identity.json');
+
 
 module.exports = {
   accessible,
@@ -32,6 +34,7 @@ module.exports = {
   'lighthouse-performance': lighthousePerformance,
   'lighthouse-pwa': lighthousePwa,
   'lighthouse-seo': lighthouseSeo,
+  identity,
   media,
   metrics,
   notifications,
