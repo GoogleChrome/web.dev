8f8c1e2c7603cd8712ac82a7b39cbae97062ae94
tomayac <steiner.thomas@gmail.com>
2021-02-15 10:20:27 +0100

    Fix variable name

--- src/lib/copy-section-links.js
+++ src/lib/copy-section-links.js
@@ -8,7 +8,7 @@ export function copySectionLinks() {
     // ToDo: Use `:is(h2, h3, h4, h5, h6)[id]` once support is better.
     .querySelectorAll('h2[id], h3[id], h4[id], h5[id], h6[id]')
     .forEach((heading) => {
-      heading.addEventListener('click', (e) => {
+      heading.addEventListener('click', (ev) => {
         // Don't jump when the '#' is clicked.
         if (ev.target.nodeName === 'A') {
           ev.preventDefault();
