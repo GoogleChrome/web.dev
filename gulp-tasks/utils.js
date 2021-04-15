29b271d44ec0b8ee0137b17269ed4cfbef949461
Michael Solati <mkslt04@gmail.com>
2021-04-15 11:46:39 -0700

    build(gulp): refactor gulpfile into tasks, fixes #3823

---
+++ gulp-tasks/utils.js
@@ -0,0 +1,21 @@
+const mozjpeg = require('imagemin-mozjpeg');
+const pngquant = require('imagemin-pngquant');
+const imagemin = require('gulp-imagemin');
+const through2 = require('through2');
+
+const isProd = process.env.ELEVENTY_ENV === 'prod';
+
+const compressImagesTransform = (pngQuality, jpegQuality) => {
+  if (!isProd) {
+    // This is the identity transform, which does nothing and just passes
+    // the images through directly.
+    return through2.obj();
+  }
+  return imagemin([
+    // @ts-ignore
+    pngquant({quality: [pngQuality, pngQuality]}),
+    mozjpeg({quality: jpegQuality * 100}),
+  ]);
+};
+
+module.exports = {compressImagesTransform};
