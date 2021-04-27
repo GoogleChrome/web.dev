bdd702b45f1bc8304573f83f9ef5580c3d933f1c
Rob Dodson <robdodson@google.com>
2021-04-27 12:24:33 -0700

    run csp reporting in dev.

--- src/site/_filters/csp-hash.js
+++ src/site/_filters/csp-hash.js
@@ -2,11 +2,8 @@ const {sha256base64} = require('../_data/lib/hash');
 const hashList = new Set();
 
 function cspHash(raw) {
-  if (process.env.ELEVENTY_ENV === 'prod') {
-    const hash = `'sha256-${sha256base64(raw)}'`;
-    hashList.add(hash);
-  }
-  return raw;
+  const hash = `'sha256-${sha256base64(raw)}'`;
+  hashList.add(hash);
 }
 
 function getHashList() {
