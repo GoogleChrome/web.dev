9712203184e49f0d4e4e61b9752683526e5525c8
Ewa Gasperowicz <devnook@gmail.com>
2021-04-27 14:41:19 +0200

    Move thumbnail to meta.yaml

--- src/site/content/en/learn/css/css.11tydata.js
+++ src/site/content/en/learn/css/css.11tydata.js
@@ -1,12 +1,16 @@
 module.exports = function () {
   return {
     layout: 'course',
-    thumbnail: 'image/jxu1OdD7LKOGIDU7jURMpSH2lyK2/fs5FxWQTb8apvFViRifm.svg',
     // The key is used to look up the data for the course in _data.
     // e.g. A course with a key of 'a11y' would have a corresponding
     // _data/courses/a11y directory.
     projectKey: 'css',
     eleventyComputed: {
+      thumbnail: (data) => {
+        const {projectKey} = data;
+        // Use thumbnail defined in the frontmatter or in the meta.yml file.
+        return data.thumbnail || data?.courses?.[projectKey]?.meta?.thumbnail;
+      },
       permalink: (data) => {
         if (process.env.ELEVENTY_ENV === 'prod') {
           return false;
