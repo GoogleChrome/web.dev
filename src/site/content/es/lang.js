58c06904f6696905ea04ef3110be6f5fefe3eaff
Ewa Gasperowicz <devnook@gmail.com>
2021-05-05 21:10:04 +0200

    lint

--- src/site/content/es/lang.js
+++ src/site/content/es/lang.js
@@ -1,4 +1,4 @@
 module.exports = {
-  lang: 'es',    // The intended language variation in this dir is es-419
+  lang: 'es', // The intended language variation in this dir is es-419
   locale: 'es_ALL',
 };
