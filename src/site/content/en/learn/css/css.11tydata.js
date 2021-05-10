9025e693802db918588a64156c5f624ab5ce5c58
Rob Dodson <robdodson@google.com>
2021-05-10 08:27:39 -0700

    soft launch learn css

--- src/site/content/en/learn/css/css.11tydata.js
+++ src/site/content/en/learn/css/css.11tydata.js
@@ -15,18 +15,6 @@ module.exports = function () {
         // Use thumbnail defined in the frontmatter or in the meta.yml file.
         return data.thumbnail || data?.courses?.[projectKey]?.meta?.thumbnail;
       },
-      permalink: (data) => {
-        if (process.env.ELEVENTY_ENV === 'prod') {
-          return false;
-        }
-
-        // I'm not sure why the output-permalinks.js eleventyComputed function
-        // isn't stripping this for me, maybe it has to do with the order that
-        // eleventy runs these in. But it's not a big deal as this is a short
-        // lived 11tydata.js file that we're going to remove when the course
-        // ships.
-        return (data.page.filePathStem + '.html').replace(/^\/en\//, '/');
-      },
     },
   };
 };
