8c1aa5e3d9a4f033f1d77575b41d43aef109bfcd
Andy Bell <hi@piccalil.li>
2021-07-08 16:45:25 +0100

    Tweak naming

--- gulp-tasks/sass.js
+++ gulp-tasks/sass.js
@@ -1,17 +1,16 @@
 const {dest, src} = require('gulp');
-const sass = require('sass');
-const sassProcessor = require('gulp-sass')(sass);
+const sassProcessor = require('gulp-sass')(require('sass'));
 
 const sourceFiles = ['./src/scss/next.scss', './src/styles/main.scss'];
 
 // Flags wether we compress the output etc
 const isProduction = process.env.NODE_ENV === 'production';
 
-const sassNext = (cb) => {
+const sass = (cb) => {
   return src(sourceFiles, {sourcemaps: !isProduction})
     .pipe(sassProcessor().on('error', sassProcessor.logError))
     .pipe(dest('./dist/css', {sourcemaps: !isProduction}))
     .on('done', cb);
 };
 
-module.exports = sassNext;
+module.exports = sass;
