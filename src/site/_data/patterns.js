1e59b2fe1fefb7f56a41391aac5e6ce4addbd08e
Ewa Gasperowicz <devnook@gmail.com>
2021-08-16 12:31:12 +0200

    Update _data fileoverview comment

--- src/site/_data/patterns.js
+++ src/site/_data/patterns.js
@@ -1,5 +1,10 @@
 const patterns = require('../../lib/patterns');
 
+/**
+ * @fileoverview This module gets the Code Patterns from the patterns.js module
+ *   and exposes them for the use in eleventy.
+ */
+
 module.exports = {
   patterns: patterns.patterns(),
   sets: patterns.sets(),
