290df06fc4bcb0347512e76f97e8159e15902eec
Andy Bell <hi@piccalil.li>
2021-09-07 12:40:34 +0100

    PR review

--- src/site/_data/design/patterns.js
+++ src/site/_data/design/patterns.js
@@ -267,7 +267,7 @@ module.exports = {
               data: {
                 title: variant.data.title,
                 extraSampleCSS:
-                  variant.data.extraSampleCSS || item.data.extraSampleCSS,
+                  variant.data.extraSampleCSS ?? item.data.extraSampleCSS,
               },
             },
           });
