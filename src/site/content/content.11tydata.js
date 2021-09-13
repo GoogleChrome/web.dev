b779f887e7af95964d00e5ecf6ca35af221f6c8c
Michael Solati <mkslt04@gmail.com>
2021-09-13 14:43:17 -0700

    feat(tags): automagically add `origin-trials` tag to posts, fixes #3311

--- src/site/content/content.11tydata.js
+++ src/site/content/content.11tydata.js
@@ -2,6 +2,20 @@ const outputPermalink = require('../../build/output-permalink');
 
 module.exports = {
   eleventyComputed: {
+    tags: (data) => {
+      // Add the following tags if necessary
+      const originTrials = 'origin-trials';
+
+      if (!Array.isArray(data.tags)) {
+        data.tags = [];
+      }
+
+      if (data.origin_trial && !data.tags.includes(originTrials)) {
+        data.tags.push(originTrials);
+      }
+
+      return data.tags;
+    },
     permalink: (data) => outputPermalink(data),
   },
 };
