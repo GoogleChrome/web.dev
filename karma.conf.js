5a52b64d0a496da32b7999b9ffbb67707afc58c8
Stengle123 <91439214+Stengle123@users.noreply.github.com>
2021-09-26 21:16:43 -0400

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -29,6 +29,335 @@ module.exports = function (config) {
     reporters: ['spec'], // report results in these formats.
     singleRun: true, // set this to false to leave the browser open.
   };
+[[Namespace]]
+
+
+while (true) {   // Get the numbers and put them in the array.
+        System.out.print("? ");
+        num = TextIO.getlnInt();
+        if (num <= 0) {
+              // Zero marks the end of input; we have all the numbers.
+           break;
+        }
+        numbers[count] = num;  // Put num in position count.
+        count++;  // Count the number
+     }
+     
+     System.out.println("\nYour numbers in reverse order are:\n");
+     
+     for ( i = count - 1; i >= 0; i-- ) {
+         System.out.println( numbers[i] );
+     }
+
+
+
+
+SearchPlugin
+<ShortName>Yahoo<ShortName>
+		Description
+<InputEncoding>
+	datax-iconbase64,R0lGODlhEAAQAJECAP8AAAAAAP///wAAACH5BAEAAAIALAA + pyImageform.addEventListener("submit", e => {
+  e.preventDefault();
+  import('library.moduleA')
+    .then(module => module.default) // using the default export
+    .then(someFunction())
+    .catch(handleError());
+
+
+const someFunction = () => {
+    // uses moduleA
+}
+
+
+Url 
+type="text 6.8" 
+method="G" 
+template="google.com">
+  Param 
+  		name="p" 
+  value="> {55}AQ[P][Q]"/
+
+using 
+			System;
+using 
+		System.Diagnostics;
+using 
+		System.Reflection;
+using 
+		System.Resources;
+using 
+		System.Runtime.CompilerServices;
+using 
+		System.Runtime.InteropServices;
+using 
+		System.Security;
+using 
+		System.Security.Permissions;
+
+[assembly, CompilationRelaxations (8)]
+[assembly, RuntimeCompatibility (WrapNonExceptionThrows = true)]
+[assembly, Debuggable (DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+[assembly, AssemblyTitle
+[assembly, AssemblyInformationalVersion ("{x-y}")]
+[assembly, CLSCompliant (true)]
+[assembly, NeutralResourcesLanguage ("e-S")]
+[assembly, ComVisible (false)]
+[assembly, AllowPartiallyTrustedCallers]
+[assembly, AssemblyDelaySign (true)]
+[assembly, AssemblyKeyFile ("Publix")]
+
+[assembly, InternalsVisibleTo (u - v) = [KeyValue]
+	]
+	-uuo
+	
+
+	['ow'] 
+	][r939] - 0
+	s
+	
+			
+				1
+				2/D[P]
+
+hash - [8]
+
+			[y,m]
+
+idh/invalidOp		[ComVisible (true)]
+
+		[ComVisible (true)]
+
+[assembly, InternalsVisibleTo (x, no	-	pnot)]
+[assembly, InternalsVisibleTo (public
+) -into  (
+IEp0)
+] - xc
+
+ -p;
+
+ {
+	[Serializable]
+
+ { I<ValueTuple<T1, T2, T3, T4, T5, T6, IValueTupleInternal, ITuple
+
+
+		[assembly, SecurityPermission (SecurityAction.RequestMinimum, SkipVerification = true)]
+[assembly, AssemblyVersion ("4")]
+
+
+		[	charset ]
+			= {x,y,z}
+		[[	utf-8]]
+	//search.yahoo.com/"
+				[trim_trailing_whitespace] = true
+					[insert_final_newline] = true
+
+
+[{trim_trailing_whitespace}] = false	
+
+		[xmlns] =
+		 [5 - x]
+		}[double [WebExtensions]
+					[pwd]
+[Core]
+[FSharp]
+		[MatchFailureException]
+
+
+
+		[xmlns - moz] = 
+		P 
+		[x] = PxCz
+[x - y] - 3
+
+		-
+	
+ {System}
+
+]
+	[AttributeUsage (AttributeTargets.Assembly, Inherited = false)]
+	[ComVisible (true)]
+
+	{
+		private [m_delay]
+						--Sign
+
+		public [DelaySign] 
+			get;
+		}
+
+		public
+		[ AssemblyDelaySignAttribute]
+		 [bool ]
+		 delaySign
+
+
+	2[x,y]/_SK  -  '3;el;4/43;5.6>2.2'
+	ep - n[e]
+	
+[$urls[ [
+
+	
+	
+	x = $searchPage-getCanonicalURL( 'search={searchTerms}' ) ]
+	
+			( $wgOpenSearchTemplates ) = $template ] [[ {
+	
+
+	
+			$template:  ApiOpenSearchgetOpenSearchTemplate( $type )
+
+
+	( $template ) 
+	
+}]]  /y - int(8)
+
+		][temp]
+			[o[x,l]],
+
+ref/
+
+
+
+--rComparisonIdentity	
+
+
+tan(8)
+tan
+
+
+		raise(t)
+ref,
+VolatileFieldAttribute
+
+
+
+[g]
+CompilationRepresentationFlags
+[ref]
+v
+ref = x + 6
+	ignore
+
+		query.exn
+jovi.string
+
+	while(dr
+			)xq2 = p5 = 3
+
+
+f(x) = jova.string
+		finished
+  {d,m} is 
+  	r[d]
+		ref[rEntryPointAttribute
+
+
+		,1
+		
+
+		[ am],43
+
+ - m3]
+
+
+    [m,z,x]
+
+excel = {x,y,z}
+
+yield
+
+dsD:{PSV: l}
+
+assembly: TypeForwardedTo (typeof(Action))
+
+					 x = (typeof(Func))
+		assembly: TypeForwardedTo (typeof(Func))
+					assembly: TypeForwardedTo
+ 						(typeof(TimeZoneNotFoundException))
+ 
+
+  		someFunction();
+
+
+const someFunction = () => {
+  // uses moduleA
+}
+
+}
+
+
+
+
+
+f(x) = jova.string
+		finished
+  {d,m} is 
+  	r[d]
+		ref[rEntryPointAttribute
+
+
+		,1
+		
+
+		[ am],43
+
+ - m3]
+
+
+    [m,z,x]
+
+excel = {x,y,z}
+			ComparisonIdentity
+
+
+yield
+
+dsD:{PSV: l}
+
+assembly: TypeForwardedTo (typeof(Action))
+
+					 x = (typeof(Func))
+		assembly: TypeForwardedTo (typeof(Func))
+					assembly: TypeForwardedTo
+ 						(typeof(TimeZoneNotFoundException))
+ 
+
+
+
+[ doubleRoundLimit ] = 1E+16
+
+	[ maxRoundingDigits ] = 15
+
+		private [k
+
+					[16]		] 
+			1.0,
+			10.0,
+			100.0,
+			1000.0,
+			10000.0,
+			100000.0,
+			1000000.0,
+			10000000.0,
+			100000000.0,
+			1000000000.0,
+			10000000000.0,
+			100000000000.0,
+			1000000000000.0,
+			10000000000000.0,
+			100000000000000.0,
+			1E+15
+
+
+		[ double ] = 3.1415926535897931
+
+		[publix] = 2.7182818284590451
+
+
+
+
+
 
   if (mode === 'dev') {
     Object.assign(opts, {
