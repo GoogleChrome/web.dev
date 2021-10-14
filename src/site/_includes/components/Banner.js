330a001cf1d1eba27866d5f607bbd552fcabe41c
Andy Bell <me@andy-bell.co.uk>
2021-10-14 17:39:57 +0100

    Page shell (#6432)
    * Implement partial banner shortcode
    
    * Add new page shell with some integration styling tweaks
    
    * skip link tweaks
    
    * Add wrapper and region to default layout
    
    * Add doc to skip link
    
    * Tweaked link styles in footer
    
    * Smaller footer links

--- src/site/_includes/components/Banner.js
+++ src/site/_includes/components/Banner.js
@@ -19,7 +19,37 @@ const md = require('markdown-it')({
   html: true, // Allow full links, e.g. with _target=blank.
 });
 
-module.exports = (content, type = 'info', location) => {
+const isDesignSystemContext = require('../../../lib/utils/is-design-system-context');
+
+/* NOTE: This component is in a transition period to support both new design system contexts
+and the existing system. Once the new design system has been *fully* rolled out, this component
+can be cleaned up with the following:
+
+1. The isDesignSystemContext conditional can be removed and code in that block should run as normal
+2. Everything from the '/// DELETE THIS WHEN ROLLOUT COMPLETE' comment *downwards* can be removed
+*/
+
+function Banner(content, type = 'info', location) {
+  if (isDesignSystemContext(this.page.filePathStem)) {
+    let utilityClasses = 'bg-state-info-bg color-core-text';
+
+    switch (type) {
+      case 'caution':
+        utilityClasses = 'bg-state-bad-bg color-core-text';
+        break;
+      case 'warning':
+        utilityClasses = 'bg-state-warn-bg color-core-text';
+        break;
+    }
+
+    return `<div role="banner" class="banner ${utilityClasses}">
+    <div class="banner__content flow">
+      ${md.renderInline(content)}
+    </div>
+    </div>`;
+  }
+
+  /// DELETE THIS WHEN ROLLOUT COMPLETE
   const locationOverride = location === 'body' ? 'w-banner--body' : '';
 
   return html`
@@ -27,4 +57,6 @@ module.exports = (content, type = 'info', location) => {
       ${md.renderInline(content)}
     </div>
   `;
-};
+}
+
+module.exports = Banner;
