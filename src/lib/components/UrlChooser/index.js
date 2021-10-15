2113bd9257df284c62bcc0d4a06b186b58f719b1
Ewa Gasperowicz <devnook@gmail.com>
2021-10-15 17:17:00 +0200

    lint

--- src/lib/components/UrlChooser/index.js
+++ src/lib/components/UrlChooser/index.js
@@ -53,7 +53,12 @@ class UrlChooser extends BaseElement {
             aria-label="Remove URL"
             @click=${this.onClearInput}
           >
-            <svg width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M19 6.41 17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41Z"/></svg>
+            <svg width="24" height="24" viewBox="0 0 24 24">
+              <path
+                fill="currentColor"
+                d="M19 6.41 17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41Z"
+              />
+            </svg>
           </button>
         </div>
         <div class="lh-controls">
