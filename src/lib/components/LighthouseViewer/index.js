1b01d25ff60bfc493af8f4f9f5eecafb9cf0b6c5
Ewa Gasperowicz <devnook@gmail.com>
2021-10-19 15:25:22 +0200

    Better error display

--- src/lib/components/LighthouseViewer/index.js
+++ src/lib/components/LighthouseViewer/index.js
@@ -15,13 +15,18 @@ class LighthouseViewer extends BaseStateElement {
   }
 
   render() {
+    const errorIcon = this.lighthouseError ? html`
+        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24" height="24" viewBox="0 0 24 24">
+          <path fill="currentColor" d="M11 9h2V7h-2m1 13c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8m0-18A10 10 0 0 0 2 12a10 10 0 0 0 10 10 10 10 0 0 0 10-10A10 10 0 0 0 12 2m-1 15h2v-6h-2v6Z"/>
+        </svg>` : ' ';
     // Classes lh-root, lh-vars come from 'lighthouse-viewer' package.
     return html`
-      <div>
-        <div class="lh-error-msg">${this.lighthouseError}</div>
-        <div class="lh-root lh-vars">
-          <div class="lighthouse-viewer"></div>
-        </div>
+      <div class="lh-error-msg text-size-0">
+        ${errorIcon}
+        ${this.lighthouseError}
+      </div>
+      <div class="lh-root lh-vars">
+        <div class="lighthouse-viewer region"></div>
       </div>
     `;
   }
