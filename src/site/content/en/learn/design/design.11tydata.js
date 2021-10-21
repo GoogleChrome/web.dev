eeef2b3911f4df9db888c1323b1e7a7774f05fef
Rachel Andrew <rachelandrew@google.com>
2021-10-21 07:38:21 +0100

    rollback to old 11tydata

--- src/site/content/en/learn/design/design.11tydata.js
+++ src/site/content/en/learn/design/design.11tydata.js
@@ -1,13 +1,9 @@
-/**
- * @fileoverview This is a generator for course data.
- */
+module.exports = function () {
+  // The key is used to look up the data for the course in _data.
+  // e.g. A course with a key of 'a11y' would have a corresponding
+  // _data/courses/a11y directory.
+  const projectKey = 'design';
 
-/**
- * @param {string} projectKey This is used to look up the data for the course
- * in _data. e.g., a course with a key of 'a11y' would have a corresponding
- * "_data/courses/a11y" directory.
- */
- module.exports = (projectKey) => {
   return {
     layout: 'course',
     projectKey,
@@ -29,13 +25,6 @@
       thumbnail: (data) => {
         // Use thumbnail defined in the frontmatter or in the meta.yml file.
         return data.thumbnail || data?.courses?.[projectKey]?.meta?.thumbnail;
-      },
-      noindex: (data) => {
-        // If the course is marked as draft, don't allow it to be indexed accidentally.
-        // In the chain below, `true` wins, so if we're draft or a specific page is marked
-        // draft, the noindex meta tags will be added.
-        const meta = data?.courses?.[projectKey]?.meta;
-        return meta?.draft || data.noindex || false;
       },
     },
   };
