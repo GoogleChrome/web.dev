11363b3f0ab49d1f0048382e0842a791fb5b30cf
Ewa Gasperowicz <devnook@gmail.com>
2021-11-03 11:24:36 +0100

    Update case studies copy

--- src/lib/components/LighthouseViewer/index.js
+++ src/lib/components/LighthouseViewer/index.js
@@ -57,6 +57,7 @@ class LighthouseViewer extends BaseStateElement {
   onStateChanged() {
     const {lighthouseResult, lighthouseError} = store.getState();
     if (lighthouseResult && lighthouseResult.run) {
+      console.log(lighthouseResult.run)
       this.generateReport(lighthouseResult.run, this.container);
     }
     this.lighthouseError = lighthouseError;
