861fef0977ab791764a8c157a117c8b0f3b4bf62
Andy Bell <hi@piccalil.li>
2021-12-01 10:50:25 +0000

    Remove references to old styles

--- src/lib/components/Snackbar/index.js
+++ src/lib/components/Snackbar/index.js
@@ -20,7 +20,6 @@
 
 import {html} from 'lit-element';
 import {BaseElement} from '../BaseElement';
-import './_styles.scss';
 
 const OPENING_ANIMATION_TIME = 150;
 const CLOSING_ANIMATION_TIME = 75;
