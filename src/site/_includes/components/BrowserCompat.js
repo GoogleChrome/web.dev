86f16ea078e15e6ae834c91d973036f27b8be475
Michael Solati <mkslt04@gmail.com>
2022-02-01 11:43:50 -0800

    refactor(BrowserCompat): use normal bp and fix label

--- src/site/_includes/components/BrowserCompat.js
+++ src/site/_includes/components/BrowserCompat.js
@@ -63,8 +63,7 @@ function BrowserCompat(feature) {
     const supportLabel = i18n(`i18n.browser_compat.browser_support`, locale);
     return `<div class="browser-compat">
       <span class="browser-compat__label">${supportLabel}:</span>
-      ${compatIcons.join('')}
-      ${sourceLink}
+      ${compatIcons.join('') + sourceLink}
     </div>
     `;
   }
