69aea5faf102874df8da2f1e189fda0fa513f4f0
Tanner <tannercdolby@gmail.com>
2022-03-08 16:05:55 -0800

    Sort list to have Latin script languages appear first and alphabetically sorted

--- src/site/_includes/components/LanguageList.js
+++ src/site/_includes/components/LanguageList.js
@@ -16,27 +16,41 @@
 const path = require('path');
 const {getTranslatedUrls, getDefaultUrl} = require('../../_filters/urls');
 const {i18n} = require('../../_filters/i18n');
-const {languageNames} = require('../../../lib/utils/language');
+const {languageNames, scriptGroups} = require('../../../lib/utils/language');
 
 module.exports = (url, lang) => {
-  const langhrefs = getTranslatedUrls(url)
-    .filter((langhref) => langhref[0] !== lang)
-    .map((langhref) => {
-      const href = langhref[1];
-      return `<a class="w-post-signpost__link"
-        translate="no"
-        lang="${langhref[0]}"
-        href="${href}">
-      ${languageNames[langhref[0]]}</a>`;
-    });
+  let langhrefs = getTranslatedUrls(url).filter(
+    (langhref) => langhref[0] !== lang,
+  );
 
   if (langhrefs.length) {
     const enHref = path.join('/', 'i18n', 'en', getDefaultUrl(url));
-    langhrefs.push(`<a class="w-post-signpost__link"
-        translate="no"
-        lang="en"
-        href="${enHref}">
-      ${languageNames['en']}</a>`);
+    langhrefs.push(['en', enHref]);
+    langhrefs = langhrefs
+      .sort((a, b) => {
+        if (
+          scriptGroups.latin.includes(a[0]) &&
+          !scriptGroups.latin.includes(b[0])
+        ) {
+          return -1;
+        }
+      })
+      .sort((a, b) => {
+        if (
+          scriptGroups.latin.includes(a[0]) &&
+          scriptGroups.latin.includes(b[0])
+        ) {
+          return a[0].localeCompare(b[0]);
+        }
+      })
+      .map((langhref) => {
+        const href = langhref[1];
+        return `<a class="w-post-signpost__link"
+          translate="no"
+          lang="${langhref[0]}"
+          href="${href}">
+        ${languageNames[langhref[0]]}</a>`;
+      });
   }
 
   const availableIn = i18n('i18n.post.available_in', lang);
