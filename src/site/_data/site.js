de4e0582bdbca930d4a9407fd07db3f852669c64
Adriana Jara <32825533+tropicadri@users.noreply.github.com>
2022-07-26 07:40:43 -0700

    Removing banner (#8410)

--- src/site/_data/site.js
+++ src/site/_data/site.js
@@ -32,9 +32,8 @@ module.exports = {
   subscribeForm:
     'https://services.google.com/fb/submissions/591768a1-61a6-4f16-8e3c-adf1661539da/',
   thumbnail: 'image/FNkVSAX8UDTTQWQkKftSgGe9clO2/uZ3hQS2EPrA9csOgkoXI.png',
-  isBannerEnabled: true,
-  banner:
-    'The PWA community is coming together for #PWASummit22. Have a great story about developing a web app? [Submit your talk today](https://sessionize.com/pwa-summit-2022)',
+  isBannerEnabled: false,
+  banner: '',
   paginationCount: PAGINATION_COUNT,
   imgixDomain: 'web-dev.imgix.net',
   bucket: 'web-dev-uploads',
