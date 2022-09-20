b5b1b523190210baa6e4725e7181c30e2018cf95
jh3y <jh3y@users.noreply.github.com>
2022-09-20 14:09:41 +0100

    feat: update eleventy config for Chrometober 2022

--- .eleventy.js
+++ .eleventy.js
@@ -297,6 +297,11 @@ module.exports = function (config) {
     }
   }
 
+  // Chrometober config
+  config.addPassthroughCopy({
+    'src/site/content/en/chrometober-2022/': 'chrometober-2022',
+  });
+
   return {
     dir: {
       input: 'src/site/content/', // we use a string path with the forward slash since windows doesn't like the paths generated from path.join
