1b6963482e67c9b75d235e9c771f524091519a82
Barry Pollard <barrypollard@google.com>
2022-11-15 20:22:07 +0000

    Handle prerender page views (#8999)
    * Handle page views for prerendering
    
    * Linting
    
    * Add another check
    
    * Fix event
    
    * Move navigation type within "if"
    
    * Refactor pageviews to analytics.js
    
    * Revert config back to prod
    
    * Review feedback
    
    * Revert back to prod

--- src/site/_data/site.js
+++ src/site/_data/site.js
@@ -54,7 +54,7 @@ module.exports = {
       COLOR_SCHEME_PREFERENCE: 'dimension7',
       WEB_VITALS_DEBUG: 'dimension8',
     },
-    version: 13,
+    version: 14,
   },
   firebase: {
     prod: {
