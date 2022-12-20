4412a57c55a38f7f577ca5442dd12dce32def1dc
Maya Janthong <45324326+mamieorine@users.noreply.github.com>
2022-12-20 19:29:50 +0000

    feat: redesign the learn page and implements the explore page (#9232)
    * feat: add explore page
    
    * feat: update card style
    
    * fix: correct locale data for explore page
    
    * feat: update layout on learn page
    
    * refactor: use script from learn page
    
    * feat(lang): add other languages for explore page
    
    * style: fix lint error
    
    * fix(lang): correct word for pl language
    
    * feat: add explore page into header navbar
    
    * fix: responsive broken
    
    * fix: responsive broken on mobile view

---
+++ src/site/content/pt/explore/explore.11tydata.js
@@ -0,0 +1,9 @@
+// =============================================================================
+// Explore OVERVIEW
+//
+// This is the context object for the explore page.
+// It helps layout cards featured on the explore page, and their ordering.
+//
+// =============================================================================
+
+module.exports = require('../../en/learn/learn.11tydata');
