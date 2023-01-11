e3c72ee303a6994c19cf4dbc9915438d19bca9ed
John <johnr@tokyo.uk>
2023-01-11 09:42:28 +0000

    Replaces the old percy targets with what currently exists from the new set

--- tools/percy/snapshots.js
+++ tools/percy/snapshots.js
@@ -8,75 +8,14 @@ const pagesToTest = [
     name: 'Home page',
   },
   {
-    url: '/learn/',
-    name: 'Learn page',
-    navPage: true,
-  },
-  {
-    url: '/accessible/',
-    name: 'Collection page',
-  },
-  {
-    url: '/measure/',
-    name: 'Measure page',
-    navPage: true,
-  },
-  {
-    url: '/blog/',
-    name: 'Blog page',
-    navPage: true,
-  },
-  {
-    url: '/test-post/',
-    name: 'Post page',
-  },
-  {
-    url: '/about/',
-    name: 'About page',
-    navPage: true,
-  },
-  {
-    url: '/codelab-avoid-invisible-text/',
-    name: 'Codelab page',
-  },
-  {
-    url: '/handbook/web-dev-components/',
-    name: 'Components page',
-  },
-  {
-    url: '/authors/',
-    name: 'Authors page',
-  },
-  {
-    url: '/authors/mgechev/',
-    name: 'Author page',
-  },
-  {
-    url: '/tags/',
-    name: 'Tags page',
-  },
-  {
-    url: '/shows/',
-    name: 'Shows page',
-  },
-  {
-    url: '/podcasts/',
-    name: 'Podcasts page',
-  },
-  {
-    url: '/newsletter/',
-    name: 'Newsletter page',
-  },
-  {
-    url: '/live/',
-    name: 'Live page',
-    navPage: true,
+    url: '/handbook/content-types/example-landing-page/',
+    name: 'Landing page',
   },
 ];
 
 // A script to navigate our app and take snapshots with Percy.
 (async () => {
-  const browser = await puppeteer.launch({
+  const browser = await puppeteer.launch( {
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
   });
 
