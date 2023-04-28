fd4d795f077eab8e8d5f5e6c8aebe9c549d73908
simgobb <sim.gobb@gmail.com>
2023-04-28 14:22:09 +0200

    chore: testing setup

--- src/lib/pages/about.js
+++ src/lib/pages/about.js
@@ -2,6 +2,12 @@
  * @fileoveriew Entrypoint for About page.
  */
 
+import '../components/EnhancedSelect';
+import '../components/FilteredElement';
+import '../components/FilteredTable';
+import '../components/CheckboxGroup';
+
 import '../components/Subscribe';
 
 import '../components/base';
+
