11c18271d64d6ccad1755a63f635c02ce80e93d6
Rachel Andrew <rachelandrew@google.com>
2023-05-02 09:20:29 +0100

    I/O promo update (#9985)

--- src/site/_data/site.js
+++ src/site/_data/site.js
@@ -35,7 +35,7 @@ module.exports = {
   thumbnail: 'image/FNkVSAX8UDTTQWQkKftSgGe9clO2/uZ3hQS2EPrA9csOgkoXI.png',
   isBannerEnabled: true,
   banner:
-    'The Chrome team is back at Google I/O on May 10! [Register Now](https://io.google/2023/?utm_source=web&utm_medium=embedded_marketing&utm_campaign=hpp_reg_banner&utm_content=)',
+    'The Chrome team is back at Google I/O on May 10! [Check out the sessions](https://io.google/2023/program/?q=web-topic,web/&utm_source=chrome&utm_medium=embedded_marketing&utm_campaign=hpp_reg_banner&utm_content=)',
   paginationCount: PAGINATION_COUNT,
   imgixDomain: 'web-dev.imgix.net',
   bucket: 'web-dev-uploads',
